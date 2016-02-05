76013758517871e3dd2cb3553837dea74262fa95
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 11:09:40 +0000

    simple|

--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -24,15 +24,23 @@ Class Data_model extends CI_Model {
 //        }
 //}
 
-function getAll() {
-    $sql = "SELECT title, author, contents FROM data WHERE id = ? AND author = ?";
-   $q =  $this->db->query($sql, array(3, 'ben oats'));
+//function getAll() {
+//    $sql = "SELECT title, author, contents FROM data WHERE id = ? AND author = ?";
+//   $q =  $this->db->query($sql, array(3, 'ben oats'));
+//
+//    if($q->num_rows() > 0) {
+//            foreach($q->result() as $row) {
+//                $data[] = $row;
+//            }
+//            return $data;
+//        }
+//}
 
-    if($q->num_rows() > 0) {
-            foreach($q->result() as $row) {
-                $data[] = $row;
-            }
-            return $data;
-        }
+function getAll() {
+    $this->db->select('title, contents');
+    $this->db->from('data');
+    $this->db->where('id', 2);
+
+    $q = $this->db->get();
 }
 }
