71ff5b9f8116550237ded9850a140d14d973d8d3
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 08:46:28 +0000

    test db

--- application/views/home.php
+++ application/views/home.php
@@ -1,9 +1,6 @@
 <p>My View</p>
-<pre>
-    <?php print_r($records); ?>
 
-</pre>
-<?php //foreach($records as $row) : ?>
-<!--    <h1>--><?php //echo $row->title; ?><!--></h1>-->
-<!---->
-<?php //endforeach; ?>
+<?php foreach($records as $row) : ?>
+    <h1><?php echo $row->title; ?></h1>
+
+<?php endforeach; ?>
