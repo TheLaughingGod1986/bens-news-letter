e122cd3a68d133b16306b0e4e1de2b4b9bdd6541
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 06:43:25 +0000

    rename class

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -1,7 +1,16 @@
 <?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ben
+ * Date: 05/02/2016
+ * Time: 06:25
+ */
 class Site extends CI_Controller
+
 {
-    function index() {
+    function index()
+    {
         echo 'hello world';
     }
 }
