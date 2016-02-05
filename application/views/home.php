c23466d19079baa03423f334f548a68e2f9d83ba
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 08:38:08 +0000

    debug

--- application/views/home.php
+++ application/views/home.php
@@ -3,7 +3,7 @@
     <?php print_r($records); ?>
 
 </pre>
-<?php foreach($records as $row) : ?>
-    <h1><?php echo $row->title; ?>></h1>
-
-<?php endforeach; ?>
+<?php //foreach($records as $row) : ?>
+<!--    <h1>--><?php //echo $row->title; ?><!--></h1>-->
+<!---->
+<?php //endforeach; ?>
