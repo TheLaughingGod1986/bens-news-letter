b9f069df4c116566f2dd9ad287e5ea552800a109
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 10:35:03 +0000

    more query

--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -1,13 +1,26 @@
 <?php
 Class Data_model extends CI_Model {
 
-    function getAll() {
-        $q = $this->db->query("SELECT * FROM data");
-        if ($q->num_rows() > 0) {
-            foreach($q->result() as $row) {
-                $data[] = $row;
-            }
-            return $data;
+//    function getAll() {
+//        $q = $this->db->query("SELECT * FROM data");
+//        if ($q->num_rows() > 0) {
+//            foreach($q->result() as $row) {
+//                $data[] = $row;
+//            }
+//            return $data;
+//        }
+//    }
+
+function getAll() {
+    $q = $this->db->het('data');
+
+    if($q->num_rows() > 0) {
+        foreach ($q->results as $row) {
+            $data[] = $row;
+        }
+        return $data;
+
         }
     }
+}
 }
