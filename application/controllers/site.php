718d12cacb704aa2ef4a0a856f54c2c884765e5c
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 06:47:05 +0000

    rename to site

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -4,10 +4,9 @@
  * Created by PhpStorm.
  * User: Ben
  * Date: 05/02/2016
- * Time: 06:25
+ * Time: 06:44
  */
-class Site extends CI_Controller
-
+class Site extends  CI_Controller
 {
     function index()
     {
