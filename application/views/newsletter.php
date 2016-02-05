7fb947f6c13ed6ae6483e9fbf0aa8d2f6912abd2
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 15:39:14 +0000

    first field

--- application/views/newsletter.php
+++ application/views/newsletter.php
@@ -4,4 +4,14 @@
 
     <?php echo form_open('email/send'); ?>
 
+    <?php echo form_close(); ?>
+
+    <?php
+$name_data = array(
+        'name' => 'name',
+        'id' => 'name',
+        'value' => set_value('name')
+
+    )
+
 </div>
