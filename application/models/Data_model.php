efdc53c2bf2e5ebd30217810b3999844e9fd7189
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 10:42:22 +0000

    chose field

--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -11,15 +11,17 @@ Class Data_model extends CI_Model {
 //        }
 //    }
 
+?
+
 function getAll() {
+    $this->bd->select('title, contents');
     $q = $this->db->get('data');
 
-    if($q->num_rows() > 0) {
-        foreach ($q->result() as $row) {
-            $data[] = $row;
+    if ($q->num_rows() > 0) {
+            foreach($q->result() as $row) {
+                $data[] = $row;
+            }
+            return $data;
         }
-        return $data;
-
-        }
-    }
+}
 }
