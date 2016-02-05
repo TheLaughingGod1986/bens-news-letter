3c48029d473fa65e09f45a631a6bb851abb7f9da
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 08:25:15 +0000

    connect to db

--- application/controllers/Sites.php
+++ application/controllers/Sites.php
@@ -10,6 +10,7 @@ class Sites extends CI_Controller
 {
     function index(){
         $this->load->model('site_model');
+        $data['records'] = $this->site_model->getAll();
     $this->load->view('home');
     }
 }
--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,9 @@
 <p>My View</p>
+<pre>
+    <?php print_r($records); ?>
+
+</pre>
+<?php foreach($record as $row) : ?>
+    <h1><?php echo $row->title; ?>></h1>
+
+<?php endforeach; ?>
