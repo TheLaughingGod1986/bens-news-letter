97c5a748201d562ec3d60a5953579a223efaeae7
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 10:50:52 +0000

    only certian var

--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -12,9 +12,21 @@ Class Data_model extends CI_Model {
 //    }
 
 
+//function getAll() {
+//    $this->db->select('title, contents');
+//    $q = $this->db->get('data');
+//
+//    if ($q->num_rows() > 0) {
+//            foreach($q->result() as $row) {
+//                $data[] = $row;
+//            }
+//            return $data;
+//        }
+//}
+
 function getAll() {
-    $this->db->select('title, contents');
-    $q = $this->db->get('data');
+    $sql = "SELECT title, author, contents FROM data WHERE id = ?";
+    $this->db->qyery($sql, 2);
 
     if ($q->num_rows() > 0) {
             foreach($q->result() as $row) {
@@ -22,5 +34,6 @@ function getAll() {
             }
             return $data;
         }
+
 }
 }
