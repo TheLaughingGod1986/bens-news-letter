21098af94bd1ad30daee556a61d3cb9c6a5324a7
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 14:38:52 +0000

    further work

---
+++ attachements/email.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['protocol'] = 'smtp';
+$config['protocol'] = 'ssh://smtp.google.com';
+$config['protocol'] = 465;
+$config['protocol'] = 'benoats@gmail.com';
+$config['protocol'] = 'Plymouth.09';
