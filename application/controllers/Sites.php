eab16da07495e15173709f792da4a234021e3d71
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 06:48:55 +0000

    new class

---
+++ application/controllers/Sites.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ben
+ * Date: 05/02/2016
+ * Time: 06:48
+ */
+class Sites extends CI_Controller
+{
+    function index()
+    {
+        echo 'hello world test';
+    }
+}
