aab378d582044e3d445246cef567178b53c3ef01
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 11:05:10 +0000

    inputing valid arguments

--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -25,15 +25,14 @@ Class Data_model extends CI_Model {
 //}
 
 function getAll() {
-    $sql = "SELECT title, author, contents FROM data WHERE id = ?";
-   $q =  $this->db->query($sql, 2);
+    $sql = "SELECT title, author, contents FROM data WHERE id = ? AND author = ?";
+   $q =  $this->db->query($sql, array(3, 'ben oats'));
 
-    ($q->num_rows() > 0) {
+    if($q->num_rows() > 0) {
             foreach($q->result() as $row) {
                 $data[] = $row;
             }
             return $data;
         }
-
 }
 }
