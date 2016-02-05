43f3d497291ee80a16b89248de5f805e54c33882
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 17:01:45 +0000

    testing form

--- application/controllers/Email.php
+++ application/controllers/Email.php
@@ -14,7 +14,6 @@ Class Email extends CI_Controller
 
     function send()
     {
-        echo "hello"; die();
         $this->load->library('email');
         $this->email->set_newline("\r\n");
 
@@ -26,11 +25,11 @@ Class Email extends CI_Controller
         $path = $this->config->item('server_root');
         $file = $path .'/news-letter/attachements/yourinfo.txt';
 
-        $this->email->attach($file);
+//        $this->email->attach($file);
 
         if($this->email->send())
         {
-            echo "success";
+            echo 'sent.';
         }
 
         else {
--- application/views/newsletter.php
+++ application/views/newsletter.php
@@ -1,27 +1,34 @@
 <h3>news letter page loaded</h3>
 <style type="text/css">
-    label {display: block}
+    label {
+        display: block
+    }
 </style>
 
 <div id="newsletter_form">
 
     <?php echo form_open('email/send'); ?>
 
-    <?php echo form_close(); ?>
-
     <?php
-$name_data = array(
+
+    $name_data = array(
         'name' => 'name',
         'id' => 'name',
         'value' => set_value('name')
     );
-?>
-    <p><label for="none">Name: </label><?php echo form_input($name_data); ?></p>
 
-    <p><label for="none">Email Address: </label>
-        <input type="text" name="email" id="email" value="<?php echo set_value('email');?>">
-    </p>
+     $email_data = array(
+         'name' => 'email',
+         'id' => 'email',
+         'value' => set_value('email')
+     );
 
+    ?>
+
+    <p><label for="name">Name: </label><?php echo form_input($name_data); ?></p>
+    <p><label for="name">Email Address: </label><?php echo form_input($email_data); ?></p>
     <p><?php echo form_submit('submit', 'submit'); ?></p>
+
+    <?php echo form_close(); ?>
 
 </div>
