50de004261d2b1e262e7f2f8660c819905476185
TheLaughingGod1986 <benoats@gmail.com>
2016-02-08 13:06:24 +0000

    always the stupid things

--- application/controllers/Sites.php
+++ application/controllers/Sites.php
@@ -21,10 +21,10 @@ class Sites extends CI_Controller
     {
         $data = array(
             'title' => $this->input->post('title'),
-            'contents' => $this->input->post('contents')
+            'content' => $this->input->post('content')
         );
 
-        $this->site_model->add_record('$data');
+        $this->site_model->add_record($data);
         $this->index();
 
     }
