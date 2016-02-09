c582ece74a5fe1f1bf4561bfa28a6237c0c264a8
TheLaughingGod1986 <benoats@gmail.com>
2016-02-09 12:58:19 +0000

    have started php templaing and css

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,6 +3,7 @@
 class Login extends CI_Controller {
     function index()
     {
-
+$data['main_content'] = 'login_form';
+        $this->load->view('includes/template', $data);
     }
 }
---
+++ application/views/includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+    <html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>no name</title>
+    <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="screen" charset="uft-8">
+</head>
+
+<body>
