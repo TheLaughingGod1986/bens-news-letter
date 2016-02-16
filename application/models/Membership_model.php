eb81af9de267b9348513610416ba7b13719483c7
Ben <benoats@gmail.com>
2016-02-16 12:53:06 +0000

    Update Membership_model.php

--- application/models/Membership_model.php
+++ application/models/Membership_model.php
@@ -3,5 +3,18 @@
  * Created by PhpStorm.
  * User: Ben
  * Date: 16/02/2016
- * Time: 12:50
+ * Time: 12:08
  */
+class Membership_model extends CI_Model {
+    function validate()
+    {
+        $this->db->where('username', $this->input->post('username'));
+        $this->db->where('password', md5($this->input->post('password')));
+        $query = $this->db->get('membership');
+
+        if($query->num_rows == 1)
+        {
+            return true;
+        }
+    }
+}
