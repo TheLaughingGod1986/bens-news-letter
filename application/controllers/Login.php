83f0504a87d9a0794153f6dbdabd321d431ee676
TheLaughingGod1986 <benoats@gmail.com>
2016-02-16 12:24:56 +0000

    test user login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,5 +7,26 @@ class Login extends CI_Controller
         $data['main_content'] = 'login_form';
         $this->load->view('includes/template', $data);
     }
-    
+
+    function validate_credentials()
+    {
+        $this->load->model('membership_model');
+        $query = $this->membership_model->validate();
+
+        if ($query) // if user cred validate
+        {
+            $data = array(
+                'username' => $this->input->post('username'),
+                'is_logged_in' => true
+            );
+
+            $this->session->set_userdata('$data');
+            redirect('sites/members_area');
+        }
+
+        else
+        {
+            $this->index();
+        }
+    }
 }
