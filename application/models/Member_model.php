eafcf72ce14398fedc15fc61f36f2778b1344ec2
TheLaughingGod1986 <benoats@gmail.com>
2016-02-16 19:04:28 +0000

    yay

--- application/models/Member_model.php
+++ application/models/Member_model.php
@@ -9,7 +9,7 @@ class Member_model extends CI_Model {
     function validate()
     {
         $this->db->where('username', $this->input->post('username'));
-        $this->db->where('password', md5($this->input->post('password')));
+        $this->db->where('password', $this->input->post('password'));
         $query = $this->db->get('membership');
 
         if($query->num_rows == 1)
