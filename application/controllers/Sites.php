cb0f9d7290d3f946c953adb67be9b13c39529306
TheLaughingGod1986 <benoats@gmail.com>
2016-02-17 21:09:22 +0000

    add css

--- application/controllers/Sites.php
+++ application/controllers/Sites.php
@@ -13,7 +13,7 @@ class Sites extends CI_Controller
         $this->load->library('pagination');
         $this->load->library('table');
 
-        $this->table->set_heading('ID', 'The Title', 'The Content');
+        $this->table->set_heading('ID', 'The Title', 'The Author', 'The Content');
 
         $config['base_url'] = 'http://benoats.co/university/news-letter/index.php/sites/index';
         $config['total_rows'] = $this->db->get('data')->num_rows();
