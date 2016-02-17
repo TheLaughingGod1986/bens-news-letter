8099d366a7d1e69bf44a7515583202a62cba8827
TheLaughingGod1986 <benoats@gmail.com>
2016-02-17 02:22:20 +0000

    test login form

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -51,7 +51,7 @@ class Login extends CI_Controller
 
         if($this->form_validation->run() == FALSE)
         {
-            $this->load->view('signup_form');
+            $this->signup();
         }
         else
         {
