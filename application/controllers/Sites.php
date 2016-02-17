c0f9454902dbc03142d3b7c8991b374348ae8980
TheLaughingGod1986 <benoats@gmail.com>
2016-02-17 21:02:44 +0000

    add css

--- application/controllers/Sites.php
+++ application/controllers/Sites.php
@@ -10,7 +10,7 @@ class Sites extends CI_Controller
 {
     function index()
     {
-        $this->load->view('includes/template', $data);
+        $this->load->view('includes/template');
         $this->load->library('pagination');
         $this->load->library('table');
 
