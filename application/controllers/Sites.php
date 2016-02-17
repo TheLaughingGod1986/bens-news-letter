b7bbd6d5acef12739f3556553812224a664ea89b
TheLaughingGod1986 <benoats@gmail.com>
2016-02-17 18:37:48 +0000

    add pagination add records

--- application/controllers/Sites.php
+++ application/controllers/Sites.php
@@ -34,30 +34,30 @@ class Sites extends CI_Controller
 
     }
 
-    function create()
-    {
-        $data = array(
-            'title' => $this->input->post('title'),
-            'content' => $this->input->post('content')
-        );
-
-        $this->site_model->add_record($data);
-        $this->index();
-    }
-
-    function update()
-    {
-        $data = array(
-            'title' => 'updated title',
-            'content' => 'content update here'
-        );
-
-        $this->site_model->update_record($data);
-    }
-
-    function delete()
-    {
-        $this->site_model->delete_row();
-        $this->index();
-    }
+//    function create()
+//    {
+//        $data = array(
+//            'title' => $this->input->post('title'),
+//            'content' => $this->input->post('content')
+//        );
+//
+//        $this->site_model->add_record($data);
+//        $this->index();
+//    }
+//
+//    function update()
+//    {
+//        $data = array(
+//            'title' => 'updated title',
+//            'content' => 'content update here'
+//        );
+//
+//        $this->site_model->update_record($data);
+//    }
+//
+//    function delete()
+//    {
+//        $this->site_model->delete_row();
+//        $this->index();
+//    }
 }
