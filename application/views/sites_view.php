ba103776f68414cb5a21d26ba376d7e17c4c500c
TheLaughingGod1986 <benoats@gmail.com>
2016-02-17 21:07:59 +0000

    add css

--- application/views/sites_view.php
+++ application/views/sites_view.php
@@ -1,5 +1,5 @@
 <head>
-    <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="screen" charset="utf-8">
+    <link rel="stylesheet" href="<?php echo base_url();?>css/table.css" type="text/css" media="screen" charset="utf-8">
 </head>
 <body>
 
