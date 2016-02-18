7370dd66164ef79f4d73aba19a21c44326c05311
TheLaughingGod1986 <benoats@gmail.com>
2016-02-18 02:55:04 +0000

    output user data

--- application/models/Member_model.php
+++ application/models/Member_model.php
@@ -9,7 +9,6 @@ class Member_model extends CI_Model {
     function validate()
     {
         $this->db->where('username', $this->input->post('username'));
-        $this->db->where('first_name', $this->input->post('first_name'));
         $this->db->where('password', md5($this->input->post('password')));
         $query = $this->db->get('membership');
 
