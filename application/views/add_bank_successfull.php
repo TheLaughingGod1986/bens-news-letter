16a3a206d81869d314254b85f685aeff93dd5793
TheLaughingGod1986 <benoats@gmail.com>
2016-02-18 17:24:40 +0000

    add create bank controller, view and model

--- application/views/add_bank_successfull.php
+++ application/views/add_bank_successfull.php
@@ -2,3 +2,4 @@
 
      <p>   <?php echo anchor('banks', 'Add Another Bank'); ?> </p>
 <!--<p> --><?php //echo anchor($this->load->view('logged_in_area', 'Profile'));?><!--</p>-->
+<?php $this->load->view('add_bank_form');?>
