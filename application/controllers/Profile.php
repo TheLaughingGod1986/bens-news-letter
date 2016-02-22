71e345a81751737111aa0563bcbb63c9f44126df
TheLaughingGod1986 <benoats@gmail.com>
2016-02-22 19:51:15 +0000

    add profile controler

---
+++ application/controllers/Profile.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ben
+ * Date: 05/02/2016
+ * Time: 06:48
+ */
+class Profile extends CI_Controller
+{
+    function index()
+    {
+
+        $this->load->view('profile_view');
+
+    }
+
+
+}
