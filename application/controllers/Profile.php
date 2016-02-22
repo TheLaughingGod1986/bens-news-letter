77d98cc0301eff32c9970296921eba6dc935ec50
TheLaughingGod1986 <benoats@gmail.com>
2016-02-22 23:47:25 +0000

    test fix

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -1,24 +1,18 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Ben
- * Date: 05/02/2016
- * Time: 06:48
- */
 class Profile extends CI_Controller
 {
     function index()
     {
         $data = array();
-        $this->load->model('profile_model');
-        $query = $this->profile_model->getbank($data);
 
-        if(!empty($query))
-        {
+        $this->load->model('Profile_model');
+
+        if ($query = $this->Profile_model->getbank()) {
             $data['records'] = $query;
         }
 
         $this->load->view('profile_view', $data);
     }
+
 }
--- application/controllers/Sites.php
+++ application/controllers/Sites.php
@@ -10,15 +10,59 @@ class Sites extends CI_Controller
 {
     function index()
     {
-        $data = array();
-        $this->load->model('profile_model');
-        $query = $this->profile_model->getbank($data);
+//        $this->load->library('pagination');
+//        $this->load->library('table');
+//
+//        $this->table->set_heading('ID', 'The Title', 'The Author', 'The Content');
+//
+//        $config['base_url'] = 'http://benoats.co/university/news-letter/index.php/sites/index';
+//        $config['total_rows'] = $this->db->get('data')->num_rows();
+//        $config['per_page'] = 10;
+//        $config['num_links'] = 20;
+//        $config['full_tag_open'] ='<div id="pagination">';
+//        $config['full_tag_close'] ='</div>';
+//
+//        $this->pagination->initialize($config);
+//
+//        $data['records'] = $this->db->get('data', $config['per_page'], $this->uri->segment(3));
+//
+//        $this->load->view('sites_view', $data);
 
-        if(!empty($query))
-        {
-            $data['records'] = $query;
-        }
+//        $data = array();
+//
+//        if ($query = $this->site_model->get_records()) {
+//            $data['records'] = $query;
+//        }
 
-        $this->load->view('profile_view', $data);
+//        $this->load->view('options_view', $data);
+//        $this->load->view('profile_view');
+
+    }
+
+    function create()
+    {
+        $data = array(
+            'title' => $this->input->post('title'),
+            'content' => $this->input->post('content')
+        );
+
+        $this->site_model->add_record($data);
+        $this->index();
+    }
+
+    function update()
+    {
+        $data = array(
+            'title' => 'updated title',
+            'content' => 'content update here'
+        );
+
+        $this->site_model->update_record($data);
+    }
+
+    function delete()
+    {
+        $this->site_model->delete_row();
+        $this->index();
     }
 }
