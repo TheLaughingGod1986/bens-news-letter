55672a7da5d2cd3e789fc2b96ee79a21470eb0e6
TheLaughingGod1986 <benoats@gmail.com>
2016-02-22 19:45:23 +0000

    display profile

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -1,1 +1,11 @@
-test 123
+<?php
+$query = $this->db->query('SELECT first_name, username, email FROM membership');
+
+foreach ($query->result() as $row)
+{
+echo $row->first_name;
+echo $row->username;
+echo $row->email;
+}
+
+echo 'Total Results: ' . $query->num_rows();
