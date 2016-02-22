7b94626ef8d0aed5753dc04b4f9d2e2c0fb47a6d
TheLaughingGod1986 <benoats@gmail.com>
2016-02-22 23:45:36 +0000

    test fix

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -9,25 +9,16 @@
 class Profile extends CI_Controller
 {
     function index()
-//    {
-//        $data = array();
-//        $this->load->model('profile_model');
-//        $query = $this->profile_model->getbank($data);
-//
-//        if(!empty($query))
-//        {
-//            $data['records'] = $query;
-//        }
-//
-//        $this->load->view('profile_view', $data);
-//    }
     {
         $data = array();
+        $this->load->model('profile_model');
+        $query = $this->profile_model->getbank($data);
 
-      if ($query = $this->site_model->get_records()) {
-        $data['records'] = $query;
-      }
+        if(!empty($query))
+        {
+            $data['records'] = $query;
+        }
 
-       $this->load->view('options_view', $data);
+        $this->load->view('profile_view', $data);
     }
 }
