fd610512144960d5184529747882ba1da57e4807
TheLaughingGod1986 <benoats@gmail.com>
2016-02-22 16:52:20 +0000

    securtity update

--- application/controllers/Members.php
+++ application/controllers/Members.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Ben
  * Date: 05/02/2016
  * Time: 06:48
  */
-
 class Members extends CI_Controller
 {
 
@@ -26,24 +26,24 @@ class Members extends CI_Controller
         $this->load->view('logged_in_area');
     }
 
-        function add_bank_account() // just for sample
-        {
-            $data['main_content'] = 'add_bank_form';
-            $this->load->view('includes/template', $data);
-            $this->load->view('add_bank_form');
-            echo 'good. you\'re logged in.';
-        }
-
-
-        function is_logged_in()
-        {
-            $is_logged_in = $this->session->userdata('is_logged_in');
-
-            if (!isset($is_logged_in) || $is_logged_in != true) {
-                echo 'lol, try again. this area is secure. MEMBERS ONLY !. please ';
-echo anchor('login/index', 'Login');
-                die();
-            }
-        }
-
+    function add_bank_account() // just for sample
+    {
+        echo 'Hi';
+        $this->session->userdata('username');
+        $data['main_content'] = 'add_bank_form';
+        $this->load->view('includes/template', $data);
     }
+
+
+    function is_logged_in()
+    {
+        $is_logged_in = $this->session->userdata('is_logged_in');
+
+        if (!isset($is_logged_in) || $is_logged_in != true) {
+            echo 'lol, try again. this area is secure. MEMBERS ONLY !. please ';
+            echo anchor('login/index', 'Login');
+            die();
+        }
+    }
+
+}
