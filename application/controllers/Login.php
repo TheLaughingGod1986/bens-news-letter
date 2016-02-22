8deaa20a5d3e146a040ee8d5948a64ed9f5909db
TheLaughingGod1986 <benoats@gmail.com>
2016-02-22 18:29:11 +0000

    fix model bug

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,11 +2,11 @@
 
 class Login extends CI_Controller
 {
-    function index()
-    {
-        $data['main_content'] = 'login_form';
-        $this->load->view('includes/template', $data);
-    }
+//    function index()
+//    {
+//        $data['main_content'] = 'login_form';
+//        $this->load->view('includes/template', $data);
+//    }
 
     function validate_credentials()
     {
@@ -59,9 +59,7 @@ class Login extends CI_Controller
         } else
         {
             $this->load->model('member_model');
-
-            if
-            ($query = $this->member_model->create_member()) {
+            if ($query = $this->member_model->create_member()) {
                 $data['main_content'] = 'signup_successfull';
                             }
             else
