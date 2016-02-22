13d69dfd812bf69c0463bcc5721451577e465485
TheLaughingGod1986 <benoats@gmail.com>
2016-02-22 16:22:25 +0000

    Im sorry, something went wrong

--- application/controllers/Members.php
+++ application/controllers/Members.php
@@ -37,7 +37,8 @@ class Members extends CI_Controller
             $is_logged_in = $this->session->userdata('is_logged_in');
 
             if (!isset($is_logged_in) || $is_logged_in != true) {
-                echo 'lol, try again. this area is secure. MEMBERS ONLY !. please <a href= "../login">Login</a>';
+                echo 'lol, try again. this area is secure. MEMBERS ONLY !. please ';
+                anchor('login/logout', 'Logout');
                 die();
             }
         }
