96da5f943a7299294402a6e69c9c839a195d64b5
TheLaughingGod1986 <benoats@gmail.com>
2016-02-22 23:20:51 +0000

    update to query

--- application/models/profile_model.php
+++ application/models/profile_model.php
@@ -2,7 +2,7 @@
 
 class Profile_model extends CI_Model {
 
-    public function getbank()  
+    public function getbank()
     {
         $query = $this->db->get('bank');
         return $query->results_array();
