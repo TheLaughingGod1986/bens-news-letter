aaebea19cd1c01d3edaca6831515bdea8d3c03d2
TheLaughingGod1986 <benoats@gmail.com>
2016-02-23 00:15:20 +0000

    test fix

--- application/models/Prof_model.php
+++ application/models/Prof_model.php
@@ -4,6 +4,6 @@ class Prof_model extends CI_Model {
     public function getbank()
     {
         $query = $this->db->get('bank');
-        return $query->results_array();  
+        return $query->result_array();
     }
 }
