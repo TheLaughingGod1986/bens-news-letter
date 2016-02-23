8df1a64b7ce8bc5a675b3a5ffd7adf12ca4802b6
TheLaughingGod1986 <benoats@gmail.com>
2016-02-23 02:54:30 +0000

    added new query

--- application/models/user_profile/Profiles_model.php
+++ application/models/user_profile/Profiles_model.php
@@ -5,8 +5,11 @@ class Profiles_model extends CI_Model
     function get_bank()
     {
 //        $query = $this->db->get('bank');
-        $query = $this->db->get $query = $this->db->query("SELECT * FROM bank WHERE mem_id = '".$this->session->userdata('id')."'");
-        return $query->result();
+//        $query = $this->db->get ("SELECT * FROM 'bank' WHERE 'mem_id' MATCH {$_SESSION['id']}");
+//        return $query->result();
 
+        $this->db->where('mem_id', $this->session->userdata("id"));
+        $query = $this->db->get('bank');
+        return $query->result();
     }
 }
