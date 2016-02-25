80b6f346358f06857c52c2801030f97a366ca43c
TheLaughingGod1986 <benoats@gmail.com>
2016-02-25 02:13:59 +0000

    test decilmal

--- application/models/bank/Bank_model.php
+++ application/models/bank/Bank_model.php
@@ -5,7 +5,7 @@ class Bank_model extends CI_Model {
     {
         $new_bank_acc_insert_data = array(
             'bank_name' => $this->input->post('bank_name'),
-            'interest' => $this->input->post('interest'),
+            'interest' => ($this->input->post('interest') / 100),
             'start_amount' => $this->input->post('start_amount'),
             'length' => $this->input->post('length'),
             'mem_id' => $this->session->userdata('id')
