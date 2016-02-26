ebcc1dd36f7828e3d38c4760fa0e24d12c3091f9
TheLaughingGod1986 <benoats@gmail.com>
2016-02-26 07:05:37 +0000

    adding date picker

--- application/views/add_bank_form.php
+++ application/views/add_bank_form.php
@@ -8,7 +8,7 @@
     echo form_input('interest', set_value('interest', 'Interest'));
     echo form_input('start_amount', set_value('start_amount', 'Starting Balance'));
     echo form_input('length', set_value('length', 'Length'));
-    echo form_input("<type ='date' name='start_date'>") ;
+    echo datefmt_format_object("<type ='date' name='start_date'>") ;
     echo form_submit('submit', 'Add Bank ')
         ?>
 </fieldset>
