05ce5dc5053da78ff14f111372f1f94743b05955
TheLaughingGod1986 <benoats@gmail.com>
2016-02-26 00:09:29 +0000

    added new query

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -1,16 +1,16 @@
 <h2>My Bank Accounts</h2>
 <?php
-if (isset($records)) : foreach($records as $row) : ?>
-<h1>Account Name</h1>
-    <p><?php echo $row->bank_name;?></p>
-    <h1>Interest Rate</h1>
-    <p><?php echo $row->interest;?></p>
-    <p>Monthly Deposits</p>
-    <p><?php echo $row->start_amount;?></p>
+if (isset($records)) : foreach ($records as $row) : ?>
+    <h2>Account Name</h2>
+    <p><?php echo $row->bank_name; ?></p>
+    <h2>Interest Rate</h2>
+    <p><?php echo $row->interest; ?></p>
+    <h2>Monthly Deposits</h2>
+    <p><?php echo $row->start_amount; ?></p>
 
 <?php endforeach; ?>
 
-<?php  else : ?>
-<h3>No Records</h3>
+<?php else : ?>
+    <h3>No Records</h3>
 
 <?php endif; ?>
