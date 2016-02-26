2392a4aa858e5571f7cb8bd9bcc96d70c34b91a9
TheLaughingGod1986 <benoats@gmail.com>
2016-02-26 08:27:06 +0000

    adding date picker

--- application/views/add_bank_form.php
+++ application/views/add_bank_form.php
@@ -5,12 +5,23 @@
     <?php
     echo form_open('banks/create_bank');
     echo form_input('bank_name', set_value('bank_name', 'Account Name'));
-    echo form_input('interest', set_value('interest', 'Interest'));
+    echo form_input('interest', set_value('interest', 'Interest Rate'));
     echo form_input('start_amount', set_value('start_amount', 'Starting Balance'));
-    echo form_input('length', set_value('length', 'Length'));
+    echo form_input('length', set_value('length', 'Valid For'));
+    echo form_input('monthly_deposits', set_value('length', 'Monthly Deposits'));
+    echo form_('monthly_deposits', set_value('length', 'Monthly Deposits'));
+
+    $options = array(
+        'small'  => 'Small Shirt',
+        'med'    => 'Medium Shirt',
+        'large'   => 'Large Shirt',
+        'xlarge' => 'Extra Large Shirt',
+    );
+
+    $shirts_on_sale = array('small', 'large');
+
+    echo form_dropdown('shirts', $options, 'large');
         ?>
-    </fieldset>
-    <fieldset>
     <input type="date" name="start_date">
       <?php echo form_submit('submit', 'Add Bank ')?>
 </fieldset>
