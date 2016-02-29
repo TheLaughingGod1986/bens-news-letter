7453d7318e5415f2e901faaf8fa4451ce5756179
TheLaughingGod1986 <benoats@gmail.com>
2016-02-29 21:56:30 +0000

    added current date

--- application/models/bank/Bank_model.php
+++ application/models/bank/Bank_model.php
@@ -1,6 +1,12 @@
 <?php
 class Bank_model extends CI_Model {
 
+    function current_date() {
+        // gets current timestamp
+        date_default_timezone_set('Asia/Manila'); // What timezone you want to be the default value for your current date.
+        return date('Y-m-d H:i:s');
+    }
+
     function create_bank()
     {
 
@@ -11,7 +17,7 @@ class Bank_model extends CI_Model {
             'length' => $this->input->post('length'),
             'start_date' => date('Y-m-d',strtotime($this->input->post('start_date'))),
             'mem_id' => $this->session->userdata('id'),
-            'account_add_date' => $this->input-> date('Y-m-d')
+            'account_add_date' => $$this->input->post('current_date')
         );
 
         $insert = $this->db->insert('bank', $new_bank_acc_insert_data);
