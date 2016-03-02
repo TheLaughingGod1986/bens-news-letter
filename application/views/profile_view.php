ee29629603052fbe249714a4c486b79b34aa4224
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 22:28:37 +0000

    adding date picker

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -1,6 +1,28 @@
-<h2>My Bank Accounts</h2>
+<h2>My Bank Accounts Details</h2>
+<?php
+if (isset($records)) : foreach ($records as $row) : ?>
+    <h2>Account Name</h2>
+    <p><?php echo $row->bank_name; ?></p>
+    <h2>Interest Rate</h2>
+    <p><?php echo $row->interest; ?></p>
+    <h2>Monthly Deposits</h2>
+    <p><?php echo $row->start_amount; ?></p>
 
+    <h2>total amount</h2>
 
-<p>Bank data</p>
-<p><?php isset($records['differenceInMonth'])?$records['differenceInMonth']:"No record found"?></p>
+    <?php
+    $i = $row->interest;
+    $o = $row->start_amount;
+    $answer = $i * $o;
+    print "<h2>$answer</h2>";
+    ;?>
+
+<?php endforeach; ?>
+
+<?php else : ?>
+    <h3>No Accounts Added </h3>
+
+<?php endif; ?>
+<!--<h1>test--><?php //print_r($var_bank_name)?><!-- </h1>-->
+
 
