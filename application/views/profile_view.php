f25cc7a85a5ac0b2a324fd61f60baeb7e22333aa
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 21:47:22 +0000

    adding date picker

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -1,38 +1,40 @@
 <h2>My Bank Accounts</h2>
 <?php
-if (isset($records)) : foreach ($records as $row) : ?>
-    <h2>Account Name</h2>
-    <p><?php echo $row->bank_name; ?></p>
-    <h2>Interest Rate</h2>
-    <p><?php echo $row->interest; ?></p>
-    <h2>Monthly Deposits</h2>
-    <p><?php echo $row->start_amount; ?></p>
+//if (isset($records)) : foreach ($records as $row) : ?>
+<!--    <h2>Account Name</h2>-->
+<!--    <p>--><?php //echo $row->bank_name; ?><!--</p>-->
+<!--    <h2>Interest Rate</h2>-->
+<!--    <p>--><?php //echo $row->interest; ?><!--</p>-->
+<!--    <h2>Monthly Deposits</h2>-->
+<!--    <p>--><?php //echo $row->start_amount; ?><!--</p>-->
+<!---->
+<!--    <h2>total amount</h2>-->
+<!---->
+<!--    --><?php
+//    $i = $row->interest;
+//    $o = $row->start_amount;
+//    $total = $i * $o;
+//
+//    print "<h2>$total</h2>";
+//    ; ?>
+<!---->
+<!--    <h2>Age</h2>-->
+<!--    <p>-->
+<!--        --><?//= isset($records['differenceInMonth']) ? $records['differenceInMonth'] : "No record found" ?>
+<!--    </p>-->
+<?php
 
-    <h2>total amount</h2>
+foreach(differenceInMonth AS $age){
 
-    <?php
-    $i = $row->interest;
-    $o = $row->start_amount;
+    echo "<p>" . $age . "</p>";
 
-    $total = $i * $o;
+}
 
-    print "<h2>$total</h2>";#
+<?php //endforeach; ?>
+<!---->
+<?php //else : ?>
+<!--    <h3>No Accounts Added </h3>-->
+<?php //endif; ?>
 
-    ;?>
-
-    <p>Bank data</p>
-    <p><?=isset($records['differenceInMonth'])?$records['differenceInMonth']:"No record found"?></p>
-
-
-
-
-
-<?php endforeach; ?>
-
-<?php else : ?>
-    <h3>No Accounts Added </h3>
-
-<?php endif; ?>
-<!--<h1>test--><?php //print_r($var_bank_name)?><!-- </h1>-->
 
 
