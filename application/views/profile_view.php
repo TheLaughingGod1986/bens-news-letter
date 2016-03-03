7232025a7b6a04785ed0047f4f78253c71335c38
TheLaughingGod1986 <benoats@gmail.com>
2016-03-03 00:55:38 +0000

    adding date picker

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -16,7 +16,7 @@ if (isset($records)) : foreach ($records as $row) : ?>
     <?php
     $y = $row->account_add_date;
     $x = $row->start_date;
-    $age_answer = $x - $y;
+    $age_answer = 12 * $y - $x;
     print "<h2>$age_answer</h2>";
     ;?>
 
