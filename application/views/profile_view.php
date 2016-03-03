92ae026626808125eb304c15a042193e7b2f8ef2
TheLaughingGod1986 <benoats@gmail.com>
2016-03-03 06:29:42 +0000

    UPDATE age of account now printing out

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -2,36 +2,37 @@
 <hr>
 <?php
 if (isset($records)) : foreach ($records as $row) : ?>
+
     <h2>Account Name</h2>
     <p><?php echo $row->bank_name; ?></p>
+
     <h2>Interest Rate</h2>
     <p><?php echo $row->interest; ?>%</p>
+
     <h2>Monthly Deposits</h2>
     <p>£<?php echo $row->start_amount; ?></p>
-<!--    <h2>Open Date</h2>-->
-<!--    <p>--><?php //echo $row->account_add_date; ?><!--</p>-->
-<!--    <h2>Start Date</h2>-->
-<!--    <p>--><?php //echo $row->start_date; ?><!--</p>-->
-
-    <h2>Total amount</h2>
-    <?php
-    $i = $row->interest;
-    $o = $row->start_amount;
-    $answer = $i * $o;
-    print "<p>£$answer</p>";; ?>
 
     <h2>Age Of Account</h2>
     <?php
-//    $z = $row->account_add_date;
-//    $y = $row->start_date;
-
-//    $date1 = $row->account_add_date;
-    $date2 = $row->start_date;
+    $start_time = $row->start_date;
     $date1 = new DateTime('now');
-    $date2 = new DateTime($date2);
-
-    echo $date1->diff($date2)->format("%y years, %m months, %d days, %h hours, %i minuets and %s seconds");
+    $date2 = new DateTime($start_time);
+    echo $date1->diff($date2)->format
+    ("%y years, %m months, %d days, %h hours, %i minuets and %s seconds");
     ?>
+
+    <h2>Total Balance To Date</h2>
+    // p : principal
+    // i : annual rate interest
+    // c: compound
+    // n : valid for
+    // R : monthly deposit
+
+    // calculate the terms that appear twice
+    $x = $i / $c;
+    $y = pow ( (1 + $x), ($n * $c) );
+
+    $vf = $p * $y + ( ($R * $y - 1 ) / $x );
 
 <?php endforeach; ?>
 
