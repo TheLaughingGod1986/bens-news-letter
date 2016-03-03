289cbd02177df744ad2d14a02e7a82c4f242d3f7
TheLaughingGod1986 <benoats@gmail.com>
2016-03-03 03:14:11 +0000

    update with age of account

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -62,7 +62,7 @@ if (isset($records)) : foreach ($records as $row) : ?>
     $date1=date_create("2013-03-15");
     $date2=date_create("2013-12-12");
     $diff=date_diff($date1,$date2);
-    print "<h2>$diff</h2>";
+    echo $diff->format('%d days');
     ?>
 
     <p>Bank data</p>
