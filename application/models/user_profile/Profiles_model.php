223a1240b6dc9b6ed96e0ae5704c767b85007389
TheLaughingGod1986 <benoats@gmail.com>
2016-03-03 00:20:28 +0000

    adding date picker

--- application/models/user_profile/Profiles_model.php
+++ application/models/user_profile/Profiles_model.php
@@ -8,4 +8,15 @@ class Profiles_model extends CI_Model
         $query = $this->db->get('bank');
         return $query->result();
     }
+
+    function get_age() {
+        $mem_id = $this->session->userdata('id');
+        $query = $this->db
+            ->select("YEAR('account_add_date') - YEAR('start_date')) + (MONTH('account_add_date') - MONTH('start_date')) AS differenceInMonth")
+            ->where('mem_id', $mem_id)
+            ->get('bank');
+
+        return $query;
+
+    }
 }
