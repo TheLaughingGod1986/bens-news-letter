b5d3180d21f541e5e96e45b8fcb2595b060d48fd
TheLaughingGod1986 <benoats@gmail.com>
2016-03-03 04:44:24 +0000

    UPDATE age of account Is now printing out !!

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -4,7 +4,7 @@ if (isset($records)) : foreach ($records as $row) : ?>
     <h2>Account Name</h2>
     <p><?php echo $row->bank_name; ?></p>
     <h2>Interest Rate</h2>
-    <p>%<?php echo $row->interest; ?></p>
+    <p><?php echo $row->interest; ?>%</p>
     <h2>Monthly Deposits</h2>
     <p>£<?php echo $row->start_amount; ?></p>
     <!--    <h2>Open Date</h2>-->
