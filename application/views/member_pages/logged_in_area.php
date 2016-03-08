b236a83bbe0d7c2e3089fd2ee4f374da2deff02b
TheLaughingGod1986 <benoats@gmail.com>
2016-03-08 13:00:37 +0000

    test

--- application/views/member_pages/logged_in_area.php
+++ application/views/member_pages/logged_in_area.php
@@ -11,10 +11,10 @@
 <body>
 
 <h2>Welcome Back, <?php echo $this->session->userdata('username'); ?>!</h2>
-<h2>Your Surname, <?php echo $this->session->userdata('last_name'); ?>!</h2>
-<h2>Your First Name, <?php echo $this->session->userdata('first_name'); ?>!</h2>
-<h2>Your Email Address, <?php echo $this->session->userdata('email_address'); ?>!</h2>
-<h2>Your Total Interest, <?php echo $this->session->userdata('money'); ?>!</h2>
+<h2>Your Surname, <?php echo $this->session->userdata('last_name'); ?></h2>
+<h2>Your First Name, <?php echo $this->session->userdata('first_name'); ?></h2>
+<h2>Your Email Address, <?php echo $this->session->userdata('email_address'); ?></h2>
+
 
 <p>This section represents the area that only logged in members can access.</p>
 
