02c6fadb040ecef0e0e8076d2fa3d72299155647
TheLaughingGod1986 <benoats@gmail.com>
2016-03-09 23:32:43 +0000

    lvl 2

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -26,7 +26,7 @@ if (isset($records)) : foreach ($records as $row) :
         $Total_balance = 1 + $y + ($r * ($y - 1) / $x);
     }
 
-    $remain = 365 - (int)$date1->diff($date2)->format("%R%a days");
+    $remain = (int)$date1->diff($date2)->format("%R%a days");
 
 
      $Int = $row->monthly_deposits  * (int)$date1->diff($date2)->format("%m");
