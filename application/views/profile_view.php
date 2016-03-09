2ea44851fba1a58daa86595b53867aff1486abaa
TheLaughingGod1986 <benoats@gmail.com>
2016-03-09 18:43:41 +0000

    new disply table for accounts

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -9,22 +9,41 @@ if (isset($records)) : foreach ($records as $row) : ?>
     $date2 = new DateTime($start_time); ?>
 
     <?php
-    $p = $row->start_amount;
-    $i = $row->interest;
-//    $c = $row->compound_frequency;
-$c = 12;
-//    $n = (int)$date1->diff($date2)->format("%m");
-$n =2;
-    $r = $row->monthly_deposits;
+//    $p = $row->start_amount;
+//    $i = $row->interest;
+////    $c = $row->compound_frequency;
+//$c = 12;
+////    $n = (int)$date1->diff($date2)->format("%m");
+//$n =2;
+//    $r = $row->monthly_deposits;
+//
+//    $x = $i / $c;
+//    $z = $n / 12;
+//    $y = pow((1 + $x), ($z * $c));
+//
+//    if ($p != 0) {
+//        $Total_Balance = $p * $y + ($r * ($y - 1) / $x);
+//    } else {
+//        $Total_Balance = 1 + $y + ($r * ($y - 1) / $x);
+//    }
+
+
+    $p = 300; // Starting amount
+    $i = 0.06; // Interest rate
+    $c = 12; // compound frequency set to monthly
+    $n = 5/12; // Current time invested set to 6 months
+    $r = 200; // Monthly investment is 200
 
     $x = $i / $c;
-    $z = $n / 12;
-    $y = pow((1 + $x), ($z * $c));
+    $y = pow((1 + $x), ($n * $c));
 
-    if ($p != 0) {
-        $Total_Balance = $p * $y + ($r * ($y - 1) / $x);
-    } else {
-        $Total_Balance = 1 + $y + ($r * ($y - 1) / $x);
+    if($p!=0)
+    {
+        $vf = $p * $y + ($r * ($y - 1) / $x);
+    }
+    else
+    {
+        $vf = 1 + $y + ($r * ($y - 1) / $x);
     }
     ?>
 
@@ -70,7 +89,7 @@ $n =2;
 
             <td>
 <!--                --><?php //echo round($Total_Balance, 2, PHP_ROUND_HALF_UP); ?>
-                <?php echo $Total_Balance; ?>
+                <?php echo $vf; ?>
             </td>
 
             <td>
