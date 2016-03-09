d0cd0b7d4ce2101cc7d5492d14d1e00c4c7ee935
TheLaughingGod1986 <benoats@gmail.com>
2016-03-09 18:50:47 +0000

    one step at the time

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -31,7 +31,7 @@ if (isset($records)) : foreach ($records as $row) : ?>
     $p = $row->start_amount;
     $i = 0.06; // Interest rate
     $c = 12; // compound frequency set to monthly
-    $n = 5/12; // Current time invested set to 6 months
+    $n = 2/12; // Current time invested set to 6 months
     $r = $row->monthly_deposits;
 
     $x = $i / $c;
