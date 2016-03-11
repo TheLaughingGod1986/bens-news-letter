176eb6ad9c825a6ea346ff04285b2090f23b8390
TheLaughingGod1986 <benoats@gmail.com>
2016-03-11 14:58:47 +0000

    add front end system

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -4,7 +4,7 @@
     <title>temping</title>
 <!--    <link rel="stylesheet" href="--><?php //echo base_url(); ?><!--css/build/style.min.css" type="text/css" media="screen"-->
 <!--          charset="uft-8">-->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css" type="text/css" media="screen" charset="uft-8">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/new.css" type="text/css" media="screen" charset="uft-8">
 <!--    <link rel="stylesheet" href="--><?php //echo base_url(); ?><!--css/style.css" type="text/css" media="screen" charset="uft-8">-->
 <!--    <link rel="stylesheet" href="--><?php //echo base_url(); ?><!--css/build/font-awesome.min.css" type="text/css" media="screen"-->
 <!--          charset="uft-8">-->
