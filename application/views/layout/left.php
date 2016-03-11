96235896a89d7d57b6adad02efaab620040f5987
TheLaughingGod1986 <benoats@gmail.com>
2016-03-11 16:17:29 +0000

    add front end system

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -12,7 +12,7 @@
 <!--</ul>-->
 
 
-<div class="col-sm-3 col-md-2 sidebar">
+<div class="sidebar">
     <ul class="nav nav-sidebar">
         <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
         <li><a href="#">Reports</a></li>
