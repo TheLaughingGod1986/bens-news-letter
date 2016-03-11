06c362f85ee68724457cf67466c3f55c84ba3ac7
TheLaughingGod1986 <benoats@gmail.com>
2016-03-11 02:27:44 +0000

    Time To Build Front-End

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -14,7 +14,7 @@ class Profile extends CI_Controller
     public function layout () {
         $this->template['header'] = $this->load->view('layout/header', $this->Front_End_data, true);
         $this->template['left'] = $this->load->view('layout/left', $this->Front_End_data, true);
-        $this->template['middle'] = $this->load->view($this->middle, $this->Front_End_data, true);
+        $this->template['middle'] = $this->load->view('profile_view', $this->Front_End_data, true);
         $this->template['footer'] = $this->load->view('layout/footer', $this->Front_End_data, true);
         $this->load->view('layout/index', $this->template);
     }
@@ -29,7 +29,7 @@ class Profile extends CI_Controller
             $data['records'] = $query;
         }
 
-        $this->load->view($this->middle = 'profile_view',$data);
+        $this->load->view($this->middle = $data);
         $this->layout();
 
 //        $this->middle = 'profile_view';
