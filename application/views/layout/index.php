1cbc5b9ebdea3aac99d61a31bb2ba4707aab616a
TheLaughingGod1986 <benoats@gmail.com>
2016-03-11 16:47:15 +0000

    fixed head and foot

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -23,9 +23,9 @@
     <?php if ($middle) echo $middle; ?>
 </div>
 
-<div class="col-sm-12">
-    <?php if ($footer) echo $footer; ?>
-</div>
+<!--<div class="col-sm-12">-->
+<!--    --><?php //if ($footer) echo $footer; ?>
+<!--</div>-->
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
