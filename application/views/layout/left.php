e0cc3998ee13d2ed460c5b89ed03368280d70e0d
TheLaughingGod1986 <benoats@gmail.com>
2016-03-11 16:29:30 +0000

    add front end system

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,29 +1,29 @@
-<ul class="nav navbar-inverse-light li-change">
-    <li class="blue"> <img border="0" alt="boo" src="http://lorempixel.com/100/70/animals"></li>
-    <li class="grey"></li>
-    <li role="presentation"><?php echo anchor('main/index', 'Home'); ?></li>
-    <li role="presentation"><a href="#">Reports</a></li>
-    <li role="presentation"><a href="#">Write / View Response</a></li>
-    <li class="grey"></li>
-    <li role="presentation"><a href="#">Your Response</a></li>
-    <li role="presentation"><?php echo anchor('main/update', 'Update Personal Details'); ?></li>
-    <li role="presentation"><a href="#">Write / View Response</a></li>
-    <li role="presentation"><a href="#">Logout</a></li>
-</ul>
+<!--<ul class="nav navbar-inverse-light li-change">-->
+<!--    <li class="blue"> <img border="0" alt="boo" src="http://lorempixel.com/100/70/animals"></li>-->
+<!--    <li class="grey"></li>-->
+<!--    <li role="presentation">--><?php //echo anchor('main/index', 'Home'); ?><!--</li>-->
+<!--    <li role="presentation"><a href="#">Reports</a></li>-->
+<!--    <li role="presentation"><a href="#">Write / View Response</a></li>-->
+<!--    <li class="grey"></li>-->
+<!--    <li role="presentation"><a href="#">Your Response</a></li>-->
+<!--    <li role="presentation">--><?php //echo anchor('main/update', 'Update Personal Details'); ?><!--</li>-->
+<!--    <li role="presentation"><a href="#">Write / View Response</a></li>-->
+<!--    <li role="presentation"><a href="#">Logout</a></li>-->
+<!--</ul>-->
 
 
-<!--<div class="sidebar">-->
-<!--    <ul class="nav nav-sidebar">-->
-<!--        <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>-->
-<!--        <li><a href="#">Reports</a></li>-->
-<!--        <li><a href="#">Analytics</a></li>-->
-<!--        <li><a href="#">Export</a></li>-->
-<!--    </ul>-->
-<!--    <ul class="nav nav-sidebar">-->
-<!--        <li><a href="">Nav item</a></li>-->
-<!--        <li><a href="">Nav item again</a></li>-->
-<!--        <li><a href="">One more nav</a></li>-->
-<!--        <li><a href="">Another nav item</a></li>-->
-<!--        <li><a href="">More navigation</a></li>-->
-<!--    </ul>-->
-<!--</div>-->
+<div class="sidebar">
+    <ul class="nav nav-sidebar">
+        <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Reports</a></li>
+        <li><a href="#">Analytics</a></li>
+        <li><a href="#">Export</a></li>
+    </ul>
+    <ul class="nav nav-sidebar">
+        <li><a href="">Nav item</a></li>
+        <li><a href="">Nav item again</a></li>
+        <li><a href="">One more nav</a></li>
+        <li><a href="">Another nav item</a></li>
+        <li><a href="">More navigation</a></li>
+    </ul>
+</div>
