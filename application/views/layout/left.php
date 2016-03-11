e97d34b90b2f261a2c13fa649145a736867e283b
TheLaughingGod1986 <benoats@gmail.com>
2016-03-11 16:28:40 +0000

    add front end system

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,15 +1,15 @@
-<!--<ul class="nav navbar-inverse-light li-change">-->
-<!--    <li class="blue"> <img border="0" alt="boo" src="http://lorempixel.com/100/70/animals"></li>-->
-<!--    <li class="grey"></li>-->
-<!--    <li role="presentation">--><?php //echo anchor('main/index', 'Home'); ?><!--</li>-->
-<!--    <li role="presentation"><a href="#">Reports</a></li>-->
-<!--    <li role="presentation"><a href="#">Write / View Response</a></li>-->
-<!--    <li class="grey"></li>-->
-<!--    <li role="presentation"><a href="#">Your Response</a></li>-->
-<!--    <li role="presentation">--><?php //echo anchor('main/update', 'Update Personal Details'); ?><!--</li>-->
-<!--    <li role="presentation"><a href="#">Write / View Response</a></li>-->
-<!--    <li role="presentation"><a href="#">Logout</a></li>-->
-<!--</ul>-->
+<ul class="nav navbar-inverse-light li-change">
+    <li class="blue"> <img border="0" alt="boo" src="http://lorempixel.com/100/70/animals"></li>
+    <li class="grey"></li>
+    <li role="presentation"><?php echo anchor('main/index', 'Home'); ?></li>
+    <li role="presentation"><a href="#">Reports</a></li>
+    <li role="presentation"><a href="#">Write / View Response</a></li>
+    <li class="grey"></li>
+    <li role="presentation"><a href="#">Your Response</a></li>
+    <li role="presentation"><?php echo anchor('main/update', 'Update Personal Details'); ?></li>
+    <li role="presentation"><a href="#">Write / View Response</a></li>
+    <li role="presentation"><a href="#">Logout</a></li>
+</ul>
 
 
 <!--<div class="sidebar">-->
