6523d7c8e216243d5228c87367871b21cda5e1ee
TheLaughingGod1986 <benoats@gmail.com>
2016-03-11 17:02:24 +0000

    fixed head and foot

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -15,13 +15,9 @@
     <?php if ($header) echo $header; ?>
 <!--</div>-->
 
-<div class="container-fluid">
-    <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
     <?php if ($left) echo $left; ?>
 </div>
-        </div>
-    </div>
 
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
     <?php if ($middle) echo $middle; ?>
