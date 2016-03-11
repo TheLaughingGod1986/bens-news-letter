fb8043ac262238765d081a9871936f5cf747bc52
TheLaughingGod1986 <benoats@gmail.com>
2016-03-11 15:55:55 +0000

    add front end system

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,6 +1,12 @@
-<!-- IE in quirks mode -->
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN">
-<title>Fixed left-sidebar</title>
-<div id="left-sidebar"> top </div>
-<div id="content"> bpottom
-</div>
+<ul class="nav navbar-inverse-light side-bar li-change">
+    <li class="blue"> <img border="0" alt="boo" src="img/greenwich_logo.png" width="100" height="70"></li>
+    <li class="grey"></li>
+    <li role="presentation"><?php echo anchor('main/index', 'Home'); ?></li>
+    <li role="presentation"><a href="#">Reports</a></li>
+    <li role="presentation"><a href="#">Write / View Response</a></li>
+    <li class="grey"></li>
+    <li role="presentation"><a href="#">Your Response</a></li>
+    <li role="presentation"><?php echo anchor('main/update', 'Update Personal Details'); ?></li>
+    <li role="presentation"><a href="#">Write / View Response</a></li>
+    <li role="presentation"><a href="#">Logout</a></li>
+</ul>
