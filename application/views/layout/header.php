4b33ab74af3b65532c1d386664f82ace6be223f7
TheLaughingGod1986 <benoats@gmail.com>
2016-03-13 13:50:58 +0000

    fixed head and foot

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -3,11 +3,17 @@
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar">Dashboard</span>
-                <span class="icon-bar">Profile</span>
-                <span class="icon-bar">Help</span>
+                <span class="icon-bar">1</span>
+                <span class="icon-bar">2</span>
+                <span class="icon-bar">3</span>
             </button>
             <?php echo anchor('home/index'); ?><img class="navbar-brand" border="0" alt="Servare" src="<?php echo base_url();?>/home/index">
+          <?php  $img = array(
+            'src' => 'home/index',
+            'alt' => 'Delete'
+            );
+          anchor('home/index', img($img));?>
+        </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Dashboard</a></li>
