dde08c18e5f467ed13e7296597db26cb425d1b77
TheLaughingGod1986 <benoats@gmail.com>
2016-03-13 14:34:39 +0000

    have fixed and fineshed accounts template

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,20 +1,24 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                    aria-expanded="false" aria-controls="navbar">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar">Dashboard</span>
                 <span class="icon-bar">Profile</span>
                 <span class="icon-bar">Help</span>
             </button>
-
-<!--            --><?php //$path = 'home';
-//            $img = '<img src="img/servare.png" alt="">';
-//            echo anchor($path, $img);
-//            ?>
-
+            <?php $path = 'home';
+            $img = '<img src="../img/servare.png" alt="">';
+            echo anchor($path, $img);
+            ?>
         </div>
 
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Help</a></li>
+            </ul>
+        </div>
     </div>
 </nav>
