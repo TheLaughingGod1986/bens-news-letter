a377eb52b98e2b5ea87cf7ba87cba72b3cdec212
TheLaughingGod1986 <benoats@gmail.com>
2016-03-13 14:20:38 +0000

    have fixed and fineshed accounts template

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,23 +1,20 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar">Dashboard</span>
                 <span class="icon-bar">Profile</span>
                 <span class="icon-bar">Help</span>
             </button>
-<!--            --><?php //echo anchor('home/index'); ?><!--<img class="navbar-brand" border="0" alt="Servare" src="--><?php //echo base_url();?><!--/home/index">-->
-           <?php $path = 'home/imdex' .$list['id'];
+
+            <?php $path = 'home/imdex';
             $img = '<img src="../img/Servare.png" alt="">';
-            echo anchor($path, $img); ?>
+            echo anchor($path, $img);
+            ?>
+            
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Dashboard</a></li>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Help</a></li>
-            </ul>
-        </div>
+
     </div>
 </nav>
