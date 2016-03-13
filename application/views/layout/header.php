8129e394e4133b56eda9a8ee08cdf780a3f3b076
TheLaughingGod1986 <benoats@gmail.com>
2016-03-13 15:02:27 +0000

    have fixed and fineshed accounts template

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -3,12 +3,12 @@
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar">Dashboard</span>
+                <span class="icon-bar"><?php echo anchor('main/update', 'Dashboard'); ?></span>
                 <span class="icon-bar">Profile</span>
                 <span class="icon-bar">Help</span>
             </button>
             <?php $path = 'home';
-            $img = '<img src="../img/servare.png" alt="">';
+            $img = '<img src="'<?php echo base_url();?>'.img/servare.png" alt="">';
             echo anchor($path, $img);
             ?>
         </div>
