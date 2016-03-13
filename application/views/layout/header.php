ee23b243865f5e2faf4d5c87f76c97cae303759e
TheLaughingGod1986 <benoats@gmail.com>
2016-03-13 13:41:20 +0000

    fixed head and foot

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -7,8 +7,12 @@
                 <span class="icon-bar">2</span>
                 <span class="icon-bar">3</span>
             </button>
-<!--            --><?php //echo anchor('home/index'); ?><!--<img class="navbar-brand" border="0" alt="Servare" src="--><?php //echo base_url();?><!--/home/index">-->
-           <?php anchor(home/index , img('home/index'));?>
+            <?php echo anchor('home/index'); ?><img class="navbar-brand" border="0" alt="Servare" src="<?php echo base_url();?>/home/index">
+          <?php  $img = array(
+            'src' => 'home/index',
+            'alt' => 'Delete'
+            );
+            anchor('home/'.$row['index'], img($img));?>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
