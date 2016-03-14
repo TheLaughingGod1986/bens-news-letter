5e12f89cd251f822de3deb8824d19e04e8ab15b2
TheLaughingGod1986 <benoats@gmail.com>
2016-03-14 09:54:31 +0000

    fixed head and foot

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -21,7 +21,7 @@ echo anchor($path, img($img));
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><?php echo anchor('main/update', 'Dashboard'); ?>1</li>
+                <li><?php echo anchor('members/members_area', 'Dashboard'); ?>1</li>
                 <li><a href="#">Profile</a></li>
                 <li><a href="#">Help</a></li>
             </ul>
