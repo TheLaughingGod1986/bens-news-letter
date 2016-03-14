75abf4180f47f49835cd968b54a0f8de9470b995
TheLaughingGod1986 <benoats@gmail.com>
2016-03-14 17:04:40 +0000

    add front end system

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Servare - Help Grow You/'r Nest</title>
+    <title>Servare - Help Grow Your Nest</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>css/new.css" type="text/css" media="screen" charset="uft-8">
     <link rel="stylesheet" href="<?php echo base_url(); ?>css/table.css" type="text/css" media="screen" charset="uft-8">
     <link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css" type="text/css" media="screen" charset="uft-8">
