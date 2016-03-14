6e0048d88678fbafca76ac8fb3d298607dbb7fd7
TheLaughingGod1986 <benoats@gmail.com>
2016-03-14 16:43:05 +0000

    add front end system

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,6 +1,6 @@
     <ul class="nav nav-sidebar">
-        <li <?php echo $this->session->userdata('username'); ?> </li>
-        <li class="active"><?php echo anchor('profile', 'Overview'); ?></span></a></li>
+<!--        <li --><?php //echo $this->session->userdata('username'); ?><!-- </li>-->
+        <li class="active"><?php echo anchor('profile', 'Overview'); ?></a></li>
         <li><?php echo anchor('members/add_bank_account', 'Add Regular Savings'); ?></li>
         <li><a href="#">Notification Accounts</a></li>
         <li><a href="#">Fixed rate Bonds</a></li>
