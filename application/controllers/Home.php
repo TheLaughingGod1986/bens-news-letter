2cb7a8af31ba9746b22c254c821ce8f18356292d
TheLaughingGod1986 <benoats@gmail.com>
2016-03-14 10:12:45 +0000

    fixed head and foot

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,10 +2,25 @@
 
 class Home extends CI_Controller
 {
+
+    var $Front_End_data = array();
+    var $template = array();
+
+    public function layout () {
+        $this->template['header'] = $this->load->view('layout/header', $this->Front_End_data, true);
+        $this->template['left'] = $this->load->view('layout/left', $this->Front_End_data, true);
+//        $this->template['middle'] = $this->load->view($this->middle, $this->Front_End_data, true);
+//        $this->template['footer'] = $this->load->view('layout/footer', $this->Front_End_data, true);
+        $this->load->view('layout/index', $this->template);
+    }
+
     function index()
     {
         $data['main_content'] = 'home_view';
-        $this->load->view('includes/template', $data);
+//        $this->load->view('includes/template', $data);
+
+        $this->template['middle'] = $this->load->view ($this->middle = 'home_view',$data, true);
+        $this->layout();
     }
 
 }
