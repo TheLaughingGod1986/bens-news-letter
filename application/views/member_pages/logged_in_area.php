55a1fbb6e19f7040ac08f3d4fb1bc2a19d243e07
TheLaughingGod1986 <benoats@gmail.com>
2016-03-14 10:23:17 +0000

    have fixed and fineshed accounts template

--- application/views/member_pages/logged_in_area.php
+++ application/views/member_pages/logged_in_area.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-<h2>Your Profile</h2>
+<h2>Your Dashboard</h2>
 <h3>Welcome Back, <?php echo $this->session->userdata('username'); ?>!</h3>
 <h3>Your Surname, <?php echo $this->session->userdata('last_name'); ?></h3>
 <h3>Your First Name, <?php echo $this->session->userdata('first_name'); ?></h3>
