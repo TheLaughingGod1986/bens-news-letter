d2cae5f3b34fc0990b400d5db11d941f7b102c52
TheLaughingGod1986 <benoats@gmail.com>
2016-03-15 12:51:38 +0000

    the final push

--- application/views/includes/footer.php
+++
@@ -1,5 +0,0 @@
-footer here
-<h4><?php echo anchor('login/logout', 'Logout'); ?></h4>
-FIX SO IF NOT LOGGED IN DO NOT SHOW
-</body>
-</html>
--- application/views/includes/template.php
+++
@@ -1,5 +0,0 @@
-<?php $this->view('includes/header'); ?>
-
-<?php $this->view($main_content); ?>
-
-<?php $this->view('includes/footer'); ?>
