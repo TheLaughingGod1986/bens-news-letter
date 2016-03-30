4342e1b581392bbaa588d5474bab102a0087865a
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 18:51:51 +0100

    change front end layout

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -18,6 +18,11 @@
                 <?php
                 if ($this->session->userdata('id')) { ?>
                     <li><?php echo anchor('login/logout', 'Logout'); ?></li>
+                    <li><?php echo anchor('members', 'Your Dashboard'); ?></li>
+                    <li><?php echo anchor('profile', 'Account Overview'); ?></li>
+                    <li><?php echo anchor('banks', 'Add Regular Savings'); ?></li>
+                    <li><a href="#">Notification Accounts<br>(coming soon)</a></li>
+                    <li><a href="#">Fixed rate Bonds<br>(coming soon)</a></li>
                     <?php
                 }
 
