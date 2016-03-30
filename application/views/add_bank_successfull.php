461b01e682bf12aa14f615d2885bf3e199ab463a
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 17:32:12 +0100

    change front end layout

--- application/views/add_bank_successfull.php
+++ application/views/add_bank_successfull.php
@@ -1,4 +1,4 @@
 <h1>well done, you added a bank</h1>
 
 <p><?php echo anchor('members/index', 'My Dashboard');; ?></p>
-<p><?php echo anchor('bank', 'Add Another Bank'); ?></p>
+<p><?php echo anchor('banks', 'Add Another Bank'); ?></p>
--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,7 +1,7 @@
 <ul class="nav nav-sidebar">
     <li><?php echo anchor('members', 'Your Dashboard'); ?></li>
     <li><?php echo anchor('profile', 'Account Overview'); ?></li>
-    <li><?php echo anchor('bank', 'Add Regular Savings'); ?></li>
+    <li><?php echo anchor('banks', 'Add Regular Savings'); ?></li>
     <li><a href="#">Notification Accounts<br>(coming soon)</a></li>
     <li><a href="#">Fixed rate Bonds<br>(coming soon)</a></li>
 </ul>
