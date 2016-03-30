4c3e37b15bdcb5af826620801137458c94bc26ff
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 17:01:26 +0100

    change front end layout

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,9 +5,7 @@ class Home extends MY_Controller
 
     function index()
     {
-
             $data['main_content'] = 'home_view';
-            $this->template['middle'] = $this->load->view ($this->middle = 'login_form',$data, true);
             $this->layout();
     }
 
