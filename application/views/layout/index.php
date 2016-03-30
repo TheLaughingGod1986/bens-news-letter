ab449b0374aaf93041c42366618f3cd75c66b250
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 19:07:47 +0100

    change front end layout

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -39,5 +39,6 @@ else {
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
 </body>
 </html>
