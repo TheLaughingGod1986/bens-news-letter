c4f9ea7406d0f0ed007cdeaad1c87043344d08c1
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 21:49:01 +0100

    change front end layout

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -69,11 +69,15 @@
 <!--        </div><!--/.nav-collapse -->-->
 <!--    </div><!--/.container-fluid -->-->
 <!--</nav>-->
-<!---->
+
 
 
 
 <!-- Fixed navbar -->
+
+    <nav id="desktop-content" class="navbar navbar-inverse-light-grey navbar-fixed-top">
+        <nav id="mobile-content" class="navbar navbar-inverse-light-grey navbar-fixed-top">
+
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
