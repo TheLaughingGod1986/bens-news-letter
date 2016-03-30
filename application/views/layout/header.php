ce9ac00dacde0949ff15b4f31b02369520d581ae
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 19:24:23 +0100

    change front end layout

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -46,9 +46,9 @@
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
+                <span class="icon-bar">1</span>
+                <span class="icon-bar">2</span>
+                <span class="icon-bar">3</span>
             </button>
             <?php
             $this->load->helper('html'); // Load HTML Helper for img()
