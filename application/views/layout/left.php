201f45e27ab19b7797d668d0e78e0f3049884509
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 17:04:43 +0100

    change front end layout

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,7 +1,7 @@
 <ul class="nav nav-sidebar">
     <li><?php echo anchor('members/members_area', 'Profile'); ?></li>
-    <li class="active"><?php echo anchor('profile', 'Account Overview'); ?></li>
+    <li><?php echo anchor('profile', 'Account Overview'); ?></li>
     <li><?php echo anchor('members/add_bank_account', 'Add Regular Savings'); ?></li>
-    <li>Notification Accounts<br>(coming soon)</li>
-    <li>Fixed rate Bonds<br>(coming soon)</li>
+    <li><a href="#">Notification Accounts<br>(coming soon)</li>
+    <li><a href="#">Fixed rate Bonds<br>(coming soon)</li>
 </ul>
