62f931c13b4c8da9528122f3e58001a70e940b9f
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 19:17:49 +0100

    change front end layout

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -43,26 +43,13 @@
     <nav class="navbar navbar-inverse-light-grey navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                    aria-expanded="false" aria-controls="navbar">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><?php echo anchor('members', 'Your Dashboard'); ?></li>
-                    <li><?php echo anchor('profile', 'Account Overview'); ?></li>
-                    <li><?php echo anchor('banks', 'Add Regular Savings'); ?></li>
-                    <li><a href="#">Notification Accounts<br>(coming soon)</a></li>
-                    <li><a href="#">Fixed rate Bonds<br>(coming soon)</a></li>
-                </ul>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
             </button>
-            <?php
-            $this->load->helper('html'); // Load HTML Helper for img()
-            $img = base_url('img/Servare.png'); // generates text: siteroot/img/servare.png
-            $path = 'home'; // link to home controller index
-            ?>
-            <div class="logo-img">
-                <?php
-                echo anchor($path, img($img));
-                ?>
-            </div>
+            <a class="navbar-brand" href="#">Project name</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
