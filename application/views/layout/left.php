bb7105d6fa868909da1aba23babb0cdaea956cde
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 23:39:00 +0100

    change front end layout

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,7 +1,7 @@
 <ul class="nav nav-sidebar">
-    <li><a href="<?php echo anchor('members', 'Your Dashboard'); ?>"></a></li>
-    <li><a href="<?php echo anchor('profile', 'Account Overview'); ?>"></a></li>
-    <li><a href="<?php echo anchor('banks', 'Add Regular Savings'); ?>"></a></li>
+    <li><a href="<?php echo anchor('members', 'Your Dashboard'); ?>"</a></li>
+    <li><a href="<?php echo anchor('profile', 'Account Overview'); ?>"</a></li>
+    <li><a href="<?php echo anchor('banks', 'Add Regular Savings'); ?>"</a></li>
     <li><a href="#">Notification Accounts<br>(coming soon)</a></li>
     <li><a href="#">Fixed rate Bonds<br>(coming soon)</a></li>
 </ul>
