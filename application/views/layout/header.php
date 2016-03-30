ee26e5653d92226d5d35a234731cfb1925ae24b1
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 18:58:17 +0100

    change front end layout

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,6 +1,16 @@
 <nav class="navbar navbar-inverse-light-grey navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <ul>
+                <li><?php echo anchor('members', 'Your Dashboard'); ?></li>
+                <li><?php echo anchor('profile', 'Account Overview'); ?></li>
+                <li><?php echo anchor('banks', 'Add Regular Savings'); ?></li>
+                <li><a href="#">Notification Accounts<br>(coming soon)</a></li>
+                <li><a href="#">Fixed rate Bonds<br>(coming soon)</a></li>
+                    </ul>
+            </button>
             <?php
             $this->load->helper('html'); // Load HTML Helper for img()
             $img = base_url('img/Servare.png'); // generates text: siteroot/img/servare.png
@@ -12,17 +22,11 @@
                 ?>
             </div>
         </div>
-
-
+        <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
                 <?php
                 if ($this->session->userdata('id')) { ?>
                     <li><?php echo anchor('login/logout', 'Logout'); ?></li>
-                    <li><?php echo anchor('members', 'Your Dashboard'); ?></li>
-                    <li><?php echo anchor('profile', 'Account Overview'); ?></li>
-                    <li><?php echo anchor('banks', 'Add Regular Savings'); ?></li>
-                    <li><a href="#">Notification Accounts<br>(coming soon)</a></li>
-                    <li><a href="#">Fixed rate Bonds<br>(coming soon)</a></li>
                     <?php
                 }
 
@@ -33,5 +37,5 @@
                 }
                 ?>
         </div>
- 
+    </div>
 </nav>
