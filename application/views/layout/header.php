c7b16bd212d71d7a1caf4475c5045cb78b683bf7
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 22:00:55 +0100

    change front end layout

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,79 +1,52 @@
-<!--<div id="desktop-content">-->
-<!--<nav class="navbar navbar-inverse-light-grey navbar-fixed-top">-->
-<!--    </div>-->
-<!---->
-<!--<div id="mobile-content">-->
-<!--    <nav class="navbar navbar-default navbar-fixed-top">-->
-<!--    </div>-->
-<!---->
-<!--    <nav class="navbar navbar-inverse-light-grey navbar-fixed-top">-->
-<!--    <div class="container-fluid">-->
-<!--        <div class="navbar-header">-->
-<!--            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">-->
-<!--                <span class="sr-only">Toggle navigation</span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--            </button>-->
-<!--            --><?php
-//            $this->load->helper('html'); // Load HTML Helper for img()
-//            $img = base_url('img/Servare.png'); // generates text: siteroot/img/servare.png
-//            $path = 'home'; // link to home controller index
-//            ?>
-<!--            <div class="logo-img">-->
-<!--                --><?php
-//                echo anchor($path, img($img));
-//                ?>
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div id="navbar" class="navbar-collapse collapse">-->
-<!---->
-<!--            <div id="desktop-content">-->
-<!--            <ul class="nav navbar-nav navbar-right">-->
-<!--                --><?php
-//                if ($this->session->userdata('id')) { ?>
-<!--                    <li>--><?php //echo anchor('login/logout', 'Logout'); ?><!--</li>-->
-<!--                    --><?php
-//                }
-//
-//                else { ?>
-<!--                    <li>--><?php //echo anchor('login', 'Login'); ?><!--</li>-->
-<!--                    <li>--><?php //echo anchor('login/signup', 'Sign up'); ?><!--</li>-->
-<!---->
-<!--                    --><?php
-//                }
-//                ?>
-<!--            </ul>-->
-<!--                </div>-->
-<!---->
-<!--            <div id="mobile-content">-->
-<!--                <ul class="nav navbar-nav navbar-right">-->
-<!--                    --><?php
-//                    if ($this->session->userdata('id')) { ?>
-<!--                        <li>--><?php //echo anchor('members', 'Your Dashboard'); ?><!--</li>-->
-<!--                        <li>--><?php //echo anchor('profile', 'Account Overview'); ?><!--</li>-->
-<!--                        <li>--><?php //echo anchor('banks', 'Add Regular Savings'); ?><!--</li>-->
-<!--                        <li><a href="#">Notification Accounts<br>(coming soon)</a></li>-->
-<!--                        <li><a href="#">Fixed rate Bonds<br>(coming soon)</a></li>-->
-<!--                        --><?php
-//                    }
-//
-//                    else { ?>
-<!---->
-<!--                        --><?php
-//                    }
-//                    ?>
-<!--                </ul>-->
-<!--            </div>-->
-<!---->
-<!--        </div><!--/.nav-collapse -->-->
-<!--    </div><!--/.container-fluid -->-->
-<!--</nav>-->
-<!---->
+<div id="desktop-content">
+<nav class="navbar navbar-inverse-light-grey navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <?php
+            $this->load->helper('html'); // Load HTML Helper for img()
+            $img = base_url('img/Servare.png'); // generates text: siteroot/img/servare.png
+            $path = 'home'; // link to home controller index
+            ?>
+            <div class="logo-img">
+                <?php
+                echo anchor($path, img($img));
+                ?>
+            </div>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+
+
+            <ul class="nav navbar-nav navbar-right">
+                <?php
+                if ($this->session->userdata('id')) { ?>
+                    <li><?php echo anchor('login/logout', 'Logout'); ?></li>
+                    <?php
+                }
+
+                else { ?>
+                    <li><?php echo anchor('login', 'Login'); ?></li>
+                    <li><?php echo anchor('login/signup', 'Sign up'); ?></li>
+
+                    <?php
+                }
+                ?>
+            </ul>
+
+        </div><!--/.nav-collapse -->
+    </div><!--/.container-fluid -->
+</nav>
+</div>
 
 
 
 <!-- Fixed navbar -->
+<div id="mobile-content">
     <nav class="navbar navbar-inverse-light-grey navbar-fixed-top">
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
@@ -97,10 +70,23 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="../navbar/">Default</a></li>
-                <li><a href="../navbar-static-top/">Static top</a></li>
-                <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+                    <?php
+                    if ($this->session->userdata('id')) { ?>
+                        <li><?php echo anchor('members', 'Your Dashboard'); ?></li>
+                        <li><?php echo anchor('profile', 'Account Overview'); ?></li>
+                        <li><?php echo anchor('banks', 'Add Regular Savings'); ?></li>
+                        <li><a href="#">Notification Accounts<br>(coming soon)</a></li>
+                        <li><a href="#">Fixed rate Bonds<br>(coming soon)</a></li>
+                        <?php
+                    }
+
+                    else { ?>
+
+                        <?php
+                    }
+                    ?>
             </ul>
         </div><!--/.nav-collapse -->
     </div>
 </nav>
+        </div>
