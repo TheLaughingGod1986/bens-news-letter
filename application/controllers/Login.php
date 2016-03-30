4dd142a3df6590d2fe583831b790a06e406b68dc
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 16:56:39 +0100

    change front end layout

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,8 +4,8 @@ class Login extends MY_Controller
 {
     function index()
     {
-        $data['main_content'] = 'login_form';
-        $this->load->view('includes/template', $data);
+        $this->middle = 'login_form';
+        $this->layout();
     }
 
     function validate_credentials()
