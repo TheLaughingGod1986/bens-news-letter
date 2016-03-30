7193dcdb329a80a777b0fc3a283618d01e4eb730
TheLaughingGod1986 <benoats@gmail.com>
2016-03-30 22:48:15 +0100

    change front end layout

--- application/views/member_pages/logged_in_area.php
+++ application/views/member_pages/logged_in_area.php
@@ -3,3 +3,5 @@
 <h3>Your Surname, <?php echo $this->session->userdata('last_name'); ?></h3>
 <h3>Your First Name, <?php echo $this->session->userdata('first_name'); ?></h3>
 <h3>Your Email Address, <?php echo $this->session->userdata('email_address'); ?></h3>
+
+<h3>Total Savings To Date £<?php echo round($grandTotal, 2, PHP_ROUND_HALF_UP); ?></h3>
