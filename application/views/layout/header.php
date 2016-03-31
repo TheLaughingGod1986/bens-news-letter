2a2d7cb8649fde7d8c258845986df498acef02e7
TheLaughingGod1986 <benoats@gmail.com>
2016-03-31 20:14:36 +0100

    add new mobile div

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-inverse-light-grey navbar-fixed-top">
-<!--    <nav class="navbar navbar-default navbar-fixed-top">-->
+<nav class="navbar navbar-inverse-light-grey navbar-fixed-top" id="desktop-content">
+    <nav class="navbar navbar-default navbar-fixed-top" id="mobile-content">
         <div class="container-fluid">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
