c20052cdcdafb649b6b7599b0670274ed6153169
TheLaughingGod1986 <benoats@gmail.com>
2016-03-31 20:25:53 +0100

    add new mobile div

--- application/views/member_pages/logged_in_area.php
+++ application/views/member_pages/logged_in_area.php
@@ -1,9 +1,9 @@
 <h2>Your Dashboard</h2>
 <hr>
-<h3>Welcome Back, <?php echo $this->session->userdata('username'); ?>!</h3>
-<h3>Your Surname, <?php echo $this->session->userdata('last_name'); ?></h3>
-<h3>Your First Name, <?php echo $this->session->userdata('first_name'); ?></h3>
-<h3>Your Email Address, <?php echo $this->session->userdata('email_address'); ?></h3>
+<h3>Welcome Back, <strong><?php echo $this->session->userdata('username'); ?></strong>!</h3>
+<h3>Your Surname, <strong><?php echo $this->session->userdata('last_name'); ?></strong></h3>
+<h3>Your First Name, <strong><?php echo $this->session->userdata('first_name'); ?></strong></h3>
+<h3>Your Email Address, <strong><?php echo $this->session->userdata('email_address'); ?></strong></h3>
 
 <?php $grandTotal = 0; ?>
 <?php
