aa492f40b0f6a7275ca56ebc1d945158aa856da5
TheLaughingGod1986 <benoats@gmail.com>
2016-04-01 16:44:22 +0100

    add new mobile div

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,6 +1,5 @@
-<h2>Home Page, nothing to see here.</h2>
-
 <div id="desktop-content">
+<div class="container">
     <div class="jumbotron">
         <h1>Navbar example</h1>
         <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
@@ -9,6 +8,7 @@
         </p>
     </div>
 </div>
+    </div>
 
 <div id="mobile-content">
     mobile view text
