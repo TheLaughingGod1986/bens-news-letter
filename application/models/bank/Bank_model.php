f81d42e29e0172265cae88a21d98d853171693a1
TheLaughingGod1986 <benoats@gmail.com>
2016-04-01 16:38:07 +0100

    add new mobile div

--- application/models/bank/Bank_model.php
+++ application/models/bank/Bank_model.php
@@ -2,7 +2,6 @@
 class Bank_model extends CI_Model {
 
     function current_date() {
-        // gets current timestamp
         date_default_timezone_set ("Europe/London"); // What timezone you want to be the default value for your current date.
         return date('Y-m-d H:i:s');
     }
