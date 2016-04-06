dc100388d9f0d208da778c3582bf918df631ea42
TheLaughingGod1986 <benoats@gmail.com>
2016-04-06 15:01:24 +0100

    add new datetinme

--- application/views/add_bank_form.php
+++ application/views/add_bank_form.php
@@ -12,7 +12,7 @@
 
     <label>Select Date Of First Deposit</label>
 
-    <p><input style="float: left" type="text" name="start_date" id="datepicker" readonly></p>
+    <p>Select Date Of First Deposit<input style="float: left" type="text" name="start_date" id="datepicker" readonly></p>
 
     <?php echo form_submit('submit', 'Add Bank ') ?>
 </fieldset>
