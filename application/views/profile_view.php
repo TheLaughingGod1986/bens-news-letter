4c62ad2eed7d5cc301c17a9da524cf8719dba227
TheLaughingGod1986 <benoats@gmail.com>
2016-04-06 23:16:36 +0100

    sorting out the queies

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -1,6 +1,7 @@
 <?php $grandTotal = 0; ?>
 <?php
-if (isset($records)) : foreach ($records as $row) :
+if (isset($records)) :
+foreach ($records as $row) :
 
     $join_date = $row->start_date;
     $date1 = new DateTime('now');
@@ -29,67 +30,67 @@ if (isset($records)) : foreach ($records as $row) :
 
     <div id="desktop-content">
         <div class="container-fluid">
-        <table border="1">
-            <tread>
-                <tr style="background: rgba(134, 240, 133, 0.31); text-align: center;">
-                    <td>Initial Deposit Date</td>
-                    <td>Interest Rate</td>
-                    <td>Monthly Deposit</td>
-                    <td>Days Till Account Maturity</td>
-                    <td>Interest After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
-                    <td>Balance After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
+            <table border="1">
+                <tread>
+                    <tr style="background: rgba(134, 240, 133, 0.31); text-align: center;">
+                        <td>Initial Deposit Date</td>
+                        <td>Interest Rate</td>
+                        <td>Monthly Deposit</td>
+                        <td>Days Till Account Maturity</td>
+                        <td>Interest After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months
+                        </td>
+                        <td>Balance After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
+                    </tr>
+                </tread>
+                <tbody>
+
+                <?php
+                // Add field values to get row total
+                $rowTotal = $Total_balance;
+
+                // Add row total to grand total
+                $grandTotal += $rowTotal;
+                ?>
+
+                <h3><?php echo $row->bank_name; ?></h3>
+                <tr style="background-color: rgba(94, 101, 255, 0.29); text-align: center;">
+
+                    <td>
+                        <strong><?php echo $newDate; ?></strong>
+
+                    </td>
+
+                    <td>
+                        <strong><?php echo $row->interest * 100; ?>%</strong> Interest
+                    </td>
+
+                    <td>
+                        <strong>£<?php echo $row->monthly_deposits; ?></strong> (PCM)
+                    </td>
+
+                    <td>
+                        <strong><?php echo $remain; ?></strong> Days Left
+                    </td>
+
+                    <td>
+                        <strong>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></strong> Interest
+                    </td>
+
+                    <td>
+                        <strong>£<?php echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?></strong>
+                    </td>
                 </tr>
-            </tread>
-            <tbody>
-
-            <?php
-            // Add field values to get row total
-            $rowTotal = $Total_balance;
-
-            // Add row total to grand total
-            $grandTotal += $rowTotal;
-            ?>
-
-            <h3><?php echo $row->bank_name; ?></h3>
-            <tr style="background-color: rgba(94, 101, 255, 0.29); text-align: center;">
-
-                <td>
-                    <strong><?php echo $newDate; ?></strong>
-
-                </td>
-
-                <td>
-                    <strong><?php echo $row->interest * 100; ?>%</strong> Interest
-                </td>
-
-                <td>
-                    <strong>£<?php echo $row->monthly_deposits; ?></strong> (PCM)
-                </td>
-
-                <td>
-                    <strong><?php echo $remain; ?></strong> Days Left
-                </td>
-
-                <td>
-                    <strong>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></strong> Interest
-                </td>
-
-                <td>
-                    <strong>£<?php echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?></strong>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-            </div>
+                </tbody>
+            </table>
+        </div>
     </div>
 
     <div id="mobile-content">
-        <div class="container-fluid">
         <table border="1">
             <tread>
                 <tr style="background: #858bf0; text-align: center;">
                     <td><h3>Maturity</h3></td>
-                <td><h3>Interest<br>(Total)</h3></td>
+                    <td><h3>Interest<br>(Total)</h3></td>
                     <td><h3>Balance<br>(Total)</h3></td>
                 </tr>
             </tread>
@@ -120,19 +121,18 @@ if (isset($records)) : foreach ($records as $row) :
             </tr>
             </tbody>
         </table>
-            </div>
     </div>
 
 <?php endforeach; ?>
-    <div id="mobile-content">
-        <div class="container-fluid">
-        <h3>Total Savings To Date £<?php echo round($grandTotal / 2, 2, PHP_ROUND_HALF_UP); ?></h3>
-        <strong>For full breakdown of your accounts please view on a Tablet or Desktop</strong>
-            </div>
-    </div>
+<div id="mobile-content">
 
-    <div id="desktop-content">
-        <div class="container-fluid">
+    <h3>Total Savings To Date £<?php echo round($grandTotal / 2, 2, PHP_ROUND_HALF_UP); ?></h3>
+    <strong>For full breakdown of your accounts please view on a Tablet or Desktop</strong>
+</div>
+</div>
+
+<div id="desktop-content">
+    <div class="container-fluid">
         <table>
             <tbody>
             <tr>
@@ -142,12 +142,11 @@ if (isset($records)) : foreach ($records as $row) :
             </tr>
             </tbody>
         </table>
-        </div>
     </div>
 
-<?php else : ?>
-    <h3>You Have No Accounts</h3>
-    <h4>Why No Add A Account?</h4>
-<?php endif; ?>
+    <?php else : ?>
+        <h3>You Have No Accounts</h3>
+        <h4>Why No Add A Account?</h4>
+    <?php endif; ?>
 
 
