8f596d63ae2c2d8fed2a122df8fe6a9c680382e0
TheLaughingGod1986 <benoats@gmail.com>
2016-04-06 14:20:31 +0100

    add new datetinme

--- application/views/add_bank_form.php
+++ application/views/add_bank_form.php
@@ -13,13 +13,13 @@
     <label>Select Date Of First Deposit</label>
     <input type="date" data-date-format="DD MMMM YYYY" name="start_date">
 
-    <p>Date: <input type="text" id="datepicker" readonly='true'></p>
+<!--    <p>Date: <input type="text" id="datepicker" readonly='true'></p>-->
     <?php echo form_submit('submit', 'Add Bank ') ?>
 
     <div data-bind="datepicker: date, datepickerMinDate: minDate, datepickerMaxDate: maxDate"></div>
 
     <div>
-        Date Selected: <span data-bind="text: date" />
+        Date Selected: <span id="datepicker" />
     </div>
 </fieldset>
 
