e8163437076921b698f9ae7dd9f9b8171528f2e8
TheLaughingGod1986 <benoats@gmail.com>
2016-04-06 14:49:54 +0100

    add new datetinme

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(function() {
     $( "#datepicker" ).datepicker({ minDate: -365, maxDate: "+0M",
         showOn: "button",
         dateFormat: 'dd-mm-yy',
-        buttonImage: "img/calendar.gif",
         showButtonPanel: true,
         buttonText: "Select date",
         inline: true
