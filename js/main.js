f2a8d6ee8b74ebed7c5d7be37f1bb60a82a7337e
TheLaughingGod1986 <benoats@gmail.com>
2016-04-06 14:16:44 +0100

    add new datetinme

--- js/main.js
+++ js/main.js
@@ -1,55 +1,54 @@
-//$(function() {
-//    $( "#datepicker" ).datepicker({ minDate: -365, maxDate: "+0M",
-//        showOn: "button",
-//        buttonImage: "img/calendar.gif",
-//        buttonImageOnly: true,
-//        buttonText: "Select date"
-//    });
-//});
+$(function() {
+    $( "#datepicker" ).datepicker({ minDate: -365, maxDate: "+0M",
+        showOn: "button",
+        buttonImage: "img/calendar.gif",
+        buttonImageOnly: true,
+        buttonText: "Select date"
+    });
+});
 
-ko.bindingHandlers.datepicker = {
-    init: function(element, valueAccessor, allBindingsAccessor) {
-
-        var options = allBindingsAccessor().datepickerOptions || {};
-        $(element).datepicker(options);
-        ko.utils.registerEventHandler(element, "change", function() {
-            var observable = valueAccessor();
-            observable($(element).datepicker("getDate"));
-        });
-        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-            $(element).datepicker("destroy");
-        });
-    },
-    update: function(element, valueAccessor) {
-        var value = ko.utils.unwrapObservable(valueAccessor()),
-            current = $(element).datepicker("getDate");
-
-        if (value - current !== 0) {
-            $(element).datepicker("setDate", value);
-        }
-    }
-};
-
-ko.bindingHandlers.datepickerMinDate = {
-    update: function(element, valueAccessor) {
-        var value = ko.utils.unwrapObservable(valueAccessor()),
-            current = $(element).datepicker("option", "minDate", value);
-    }
-};
-
-ko.bindingHandlers.datepickerMaxDate = {
-    update: function(element, valueAccessor) {
-        var value = ko.utils.unwrapObservable(valueAccessor()),
-            current = $(element).datepicker("option", "maxDate", value);
-    }
-};
-
-var viewModel = {
-    date: ko.observable(new Date()),
-    //minDate: ko.observable(new Date()),
-    //maxDate: ko.observable(new Date()),
-    minDate: -365,
-    maxDate: "+0M"
-};
-
-ko.applyBindings(viewModel);
+//ko.bindingHandlers.datepicker = {
+//    init: function(element, valueAccessor, allBindingsAccessor) {
+//
+//        var options = allBindingsAccessor().datepickerOptions || {};
+//        $(element).datepicker(options);
+//        ko.utils.registerEventHandler(element, "change", function() {
+//            var observable = valueAccessor();
+//            observable($(element).datepicker("getDate"));
+//        });
+//        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+//            $(element).datepicker("destroy");
+//        });
+//    },
+//    update: function(element, valueAccessor) {
+//        var value = ko.utils.unwrapObservable(valueAccessor()),
+//            current = $(element).datepicker("getDate");
+//
+//        if (value - current !== 0) {
+//            $(element).datepicker("setDate", value);
+//        }
+//    }
+//};
+//
+//ko.bindingHandlers.datepickerMinDate = {
+//    update: function(element, valueAccessor) {
+//        var value = ko.utils.unwrapObservable(valueAccessor()),
+//            current = $(element).datepicker("option", "minDate", value);
+//    }
+//};
+//
+//ko.bindingHandlers.datepickerMaxDate = {
+//    update: function(element, valueAccessor) {
+//        var value = ko.utils.unwrapObservable(valueAccessor()),
+//            current = $(element).datepicker("option", "maxDate", value);
+//    }
+//};
+//
+//var viewModel = {
+//    date: ko.observable(new Date()),
+//
+//    minDate: -365,
+//    maxDate: "+0M"
+//};
+//
+//ko.applyBindings(viewModel);
