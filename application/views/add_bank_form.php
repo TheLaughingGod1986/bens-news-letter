3d6e56d0159aa68ad9d3ab77cba20e006f725586
TheLaughingGod1986 <benoats@gmail.com>
2016-04-06 15:24:35 +0100

    add new datetinme

--- application/views/add_bank_form.php
+++ application/views/add_bank_form.php
@@ -5,8 +5,14 @@
     <legend>Account Information</legend>
     <?php
     echo form_open('banks/create_bank');
+    $account_name = array(
+        'name' => 'bank_name',
+        'id' => 'bank_name',
+        'placeholder' => 'Account Name'
+    );
+    echo form_input($account_name);
 //    echo form_input('bank_name', set_value('bank_name', 'Account Name'));
-    echo form_input('bank_name','','placeholder=Account_Name');
+//    echo form_input('bank_name','','placeholder=Account_Name');
     echo form_input('interest', set_value('interest', 'Interest Rate'));
     echo form_input('monthly_deposits', set_value('monthly_deposits', 'Monthly Deposits'));
     ?>
