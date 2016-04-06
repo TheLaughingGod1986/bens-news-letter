0774abc90ec3e15c13c4407f786f41d8d61e3c2f
TheLaughingGod1986 <benoats@gmail.com>
2016-04-06 13:46:09 +0100

    add new datetinme

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
     $( "#datepicker" ).datepicker({ minDate: -365, maxDate: "+0M",
         showOn: "button",
-        buttonImage: "www.benoats.co/university/savings/img/calendar.gif",
+        //buttonImage: "img/calendar.gif",
         buttonImageOnly: true,
         buttonText: "Select date"
     });
