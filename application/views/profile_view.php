fab38a1a3b90ad977457941d5c98e23ab60d4da9
TheLaughingGod1986 <benoats@gmail.com>
2016-04-06 22:59:22 +0100

    sorting out the queies

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -28,66 +28,65 @@ if (isset($records)) : foreach ($records as $row) :
     ?>
 
     <div id="desktop-content">
-    <table border="1">
-        <tread>
-            <tr style="background: rgba(134, 240, 133, 0.31); text-align: center;">
-                <td>Initial Deposit Date</td>
-                <td>Interest Rate</td>
-                <td>Monthly Deposit</td>
-                <td>Days Till Account Maturity</td>
-                <td>Interest After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
-                <td>Balance After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
+        <table border="1">
+            <tread>
+                <tr style="background: rgba(134, 240, 133, 0.31); text-align: center;">
+                    <td>Initial Deposit Date</td>
+                    <td>Interest Rate</td>
+                    <td>Monthly Deposit</td>
+                    <td>Days Till Account Maturity</td>
+                    <td>Interest After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
+                    <td>Balance After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
+                </tr>
+            </tread>
+            <tbody>
+
+            <?php
+            // Add field values to get row total
+            $rowTotal = $Total_balance;
+
+            // Add row total to grand total
+            $grandTotal += $rowTotal;
+            ?>
+
+            <h3><?php echo $row->bank_name; ?></h3>
+            <tr style="background-color: rgba(94, 101, 255, 0.29); text-align: center;">
+
+                <td>
+                    <strong><?php echo $newDate; ?></strong>
+
+                </td>
+
+                <td>
+                    <strong><?php echo $row->interest * 100; ?>%</strong> Interest
+                </td>
+
+                <td>
+                    <strong>£<?php echo $row->monthly_deposits; ?></strong> (PCM)
+                </td>
+
+                <td>
+                    <strong><?php echo $remain; ?></strong> Days Left
+                </td>
+
+                <td>
+                    <strong>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></strong> Interest
+                </td>
+
+                <td>
+                    <strong>£<?php echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?></strong>
+                </td>
             </tr>
-        </tread>
-        <tbody>
-
-        <?php
-        // Add field values to get row total
-        $rowTotal = $Total_balance;
-
-        // Add row total to grand total
-        $grandTotal += $rowTotal;
-        ?>
-
-        <h3><?php echo $row->bank_name; ?></h3>
-        <tr style="background-color: rgba(94, 101, 255, 0.29); text-align: center;">
-
-            <td>
-                <strong><?php echo $newDate ; ?></strong>
-
-            </td>
-
-            <td>
-                <strong><?php echo $row->interest * 100; ?>%</strong> Interest
-            </td>
-
-            <td>
-                <strong>£<?php echo $row->monthly_deposits; ?></strong> (PCM)
-            </td>
-
-            <td>
-                <strong><?php echo $remain; ?></strong> Days Left
-            </td>
-
-            <td>
-                <strong>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></strong> Interest
-            </td>
-
-            <td>
-                <strong>£<?php echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?></strong>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-        </div>
+            </tbody>
+        </table>
+    </div>
 
     <div id="mobile-content">
         <table border="1">
             <tread>
                 <tr style="background: #858bf0; text-align: center;">
                     <td><h3>Maturity</h3></td>
-                    <td><h3>Interest <br>
-                        (Total)</h3></td>
+                <td><h3>Interest<br>(Total)</h3></td>
                     <td><h3>Balance<br>(Total)</h3></td>
                 </tr>
             </tread>
@@ -121,21 +120,21 @@ if (isset($records)) : foreach ($records as $row) :
     </div>
 
 <?php endforeach; ?>
-<div id="mobile-content">
-            <h3>Total Savings To Date £<?php echo round($grandTotal / 2, 2, PHP_ROUND_HALF_UP); ?></h3>
-    <strong>For full breakdown of your accounts please view on a Tablet or Desktop</strong>
+    <div id="mobile-content">
+        <h3>Total Savings To Date £<?php echo round($grandTotal / 2, 2, PHP_ROUND_HALF_UP); ?></h3>
+        <strong>For full breakdown of your accounts please view on a Tablet or Desktop</strong>
     </div>
 
-<div id="desktop-content">
-    <table>
-        <tbody>
-    <tr>
-        <td>
-            <h3>Total Savings To Date £<?php echo round($grandTotal / 2, 2, PHP_ROUND_HALF_UP); ?></h3>
-        </td>
-    </tr>
-    </tbody>
-    </table>
+    <div id="desktop-content">
+        <table>
+            <tbody>
+            <tr>
+                <td>
+                    <h3>Total Savings To Date £<?php echo round($grandTotal / 2, 2, PHP_ROUND_HALF_UP); ?></h3>
+                </td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 
 <?php else : ?>
