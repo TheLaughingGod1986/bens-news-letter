a76b5e55790a763e57336410f32bdeb3216872c6
TheLaughingGod1986 <benoats@gmail.com>
2016-04-06 19:21:15 +0100

    add new datetinme

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,36 +8,63 @@ class Login extends MY_Controller
         $this->layout();
     }
 
-    function validate_credentials()
+    function logout()
     {
-        $this->load->model('members/member_model');
-        $query = $this->member_model->validate();
-
-        if ($query) // if user cred validate the user session start
-        {
-            $data = array(
-                'username' => $query->username,
-                'id' => $query->id,
-                'password' => $query->password,
-                'first_name' => $query->first_name,
-                'last_name' => $query->last_name,
-                'email_address' => $query->email_address,
-                'is_logged_in' => true
-            );
-
-            $this->session->set_userdata($data);
-
-            redirect('members/index');
-        } else {
-            $this->index();
-            echo 'Incorrect Password or Username';
-        }
+        $this->session->sess_destroy();
+        redirect('home');
+        $this->index();
     }
 
     function signup()
     {
         $this->middle = 'signup_form';
         $this->layout();
+    }
+
+    function validate_credentials()
+    {
+//        $this->load->model('members/member_model');
+//        $query = $this->member_model->validate();
+
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('email_address', 'email_address', 'trim|required');
+        $this->form_validation->set_rules('password', 'password', 'trim|required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->middle = 'signup_form'; // return page will validation error
+            $this->layout();
+        }
+
+        else {
+            $this->load->model('members/member_model');
+            if ($query = $this->member_model->validate()) {
+                $this->middle = 'member_pages/logged_in_area';
+                $this->layout();
+            } else {
+                echo "somthing went wrong";
+            }
+        }
+
+//        if ($query) // if user cred validate the user session start
+//        {
+//            $data = array(
+//                'username' => $query->username,
+//                'id' => $query->id,
+//                'password' => $query->password,
+//                'first_name' => $query->first_name,
+//                'last_name' => $query->last_name,
+//                'email_address' => $query->email_address,
+//                'is_logged_in' => true
+//            );
+//
+//            $this->session->set_userdata($data);
+//
+//            redirect('members/index');
+//        } else {
+//            $this->index();
+//            echo 'Incorrect Password or Username';
+//        }
     }
 
     function create_member()
@@ -66,11 +93,6 @@ class Login extends MY_Controller
         }
     }
 
-    function logout()
-    {
-        $this->session->sess_destroy();
-        redirect('home');
-        $this->index();
-    }
+
 
 }
