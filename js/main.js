9409b7bceef44888faee143316b37d02f826ed8f
TheLaughingGod1986 <benoats@gmail.com>
2016-04-06 13:30:51 +0100

    add new datetinme

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 $(function() {
-    $( "#datepicker" ).datepicker({
-        minDate: -365,
-        maxDate: "+0M"
-    });
+    $( "#datepicker" ).datepicker({ minDate: -365, maxDate: "+0M" });
 });
 
