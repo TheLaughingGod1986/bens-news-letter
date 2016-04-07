d5f634ff1276928da01badb3cbaa74f6b5419d53
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 22:31:09 +0100

    tidy up front page

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -2,7 +2,7 @@
     <h2>Log In Now!</h2>
     <hr>
     <div id="desktop-content">
-        <div class="col-sm-7">
+        <div class="col-sm-5">
             <?php
             $this->load->helper('html');
             $img = base_url('img/cash-flow.jpg');
@@ -38,6 +38,30 @@
 
                 <?php echo validation_errors('<p class="error"/>'); ?>
             </div>
+        </div>
+    </div>
+    <div id="mobile-content">
+        <div id="login_form">
+            <?php
+            echo form_open('login/validate_credentials');
+            $email_addresse = array(
+                'name' => 'email_address',
+                'id' => 'email_address',
+                'placeholder' => 'Please Enter Your Email Address',
+            );
+            $password = array(
+                'name' => 'password',
+                'id' => 'password',
+                'placeholder' => 'Please Enter Your Password',
+            );
+            echo "<legend>Login Details</legend>";
+            echo form_input($email_addresse);
+            echo form_password($password);
+            echo form_submit('submit', 'Login');
+            echo anchor('login/signup', 'Create Account');
+            ?>
+
+            <?php echo validation_errors('<p class="error"/>'); ?>
         </div>
     </div>
 </div>
