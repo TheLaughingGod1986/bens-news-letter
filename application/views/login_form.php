74ff5dea41a06d57f9d31cb48b54550dc41a00b4
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 22:24:28 +0100

    tidy up front page

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -13,30 +13,30 @@
                 echo anchor($path, img($img));
                 ?>
             </div>
+        </div>
 
-            <div class="col-sm-4">
-                <div id="login_form">
-                    <?php
-                    echo form_open('login/validate_credentials');
-                    $email_addresse = array(
-                        'name' => 'email_address',
-                        'id' => 'email_address',
-                        'placeholder' => 'Please Enter Your Email Address',
-                    );
-                    $password = array(
-                        'name' => 'password',
-                        'id' => 'password',
-                        'placeholder' => 'Please Enter Your Password',
-                    );
-                    echo "<legend>Login Details</legend>";
-                    echo form_input($email_addresse);
-                    echo form_password($password);
-                    echo form_submit('submit', 'Login');
-                    echo anchor('login/signup', 'Create Account');
-                    ?>
+        <div class="col-sm-4">
+            <div id="login_form">
+                <?php
+                echo form_open('login/validate_credentials');
+                $email_addresse = array(
+                    'name' => 'email_address',
+                    'id' => 'email_address',
+                    'placeholder' => 'Please Enter Your Email Address',
+                );
+                $password = array(
+                    'name' => 'password',
+                    'id' => 'password',
+                    'placeholder' => 'Please Enter Your Password',
+                );
+                echo "<legend>Login Details</legend>";
+                echo form_input($email_addresse);
+                echo form_password($password);
+                echo form_submit('submit', 'Login');
+                echo anchor('login/signup', 'Create Account');
+                ?>
 
-                    <?php echo validation_errors('<p class="error"/>'); ?>
-                </div>
+                <?php echo validation_errors('<p class="error"/>'); ?>
             </div>
         </div>
     </div>
