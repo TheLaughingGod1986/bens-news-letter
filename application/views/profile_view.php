a84556b72b6bfcf5dcdc2e53e3d9483a9f567175
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 19:15:59 +0100

    sorting out the queies

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -3,61 +3,61 @@
     <hr>
         <?php $grandTotal = 0; ?>
         <?php
-//        if (isset($records)) :
-//        foreach ($records as $row) :
+        if (isset($records)) :
+        foreach ($records as $row) :
 
-//            $join_date = $row->start_date;
-//            $date1 = new DateTime('now');
-//            $date2 = new DateTime($join_date);
-//
-//            $p = $row->start_amount;
-//            $i = $row->interest;
-//            $c = 12; // compound frequency set to monthly
-//            $n = ((int)$date1->diff($date2)->format("%m")) / 12;
-//            $r = $row->monthly_deposits;
-//            $x = $i / $c;
-//            $y = pow((1 + $x), ($n * $c));
-//            $Total_balance = $p * $y + ($r * (1 + $x) * ($y - 1) / $x);
-//
-//            $remain = 365 - $date1->diff($date2)->format("%a days");
-//
-//            $Total_Deposits = ($row->monthly_deposits * (int)$date1->diff($date2)->format("%m")) + $row->start_amount;
-//            $Total_Int = $Total_balance - $Total_Deposits;
-//
-//            $originalDate = $row->start_date;
-//            $newDate = date("jS \of F Y", strtotime($originalDate));
-//
-//            // Add field values to get row total
-//            $rowTotal = $Total_balance;
-//
-//            // Add row total to grand total
-//            $grandTotal += $rowTotal;
-//            ?>
-<!--            <div id="desktop-content">-->
-<!--    <h3>Account: --><?php //echo $row->bank_name; ?><!--</h3>-->
-<!--            <table border="1">-->
-<!--                <tread>-->
-<!--                    <tr style="background: rgba(134, 240, 133, 0.31); text-align: center;">-->
-<!--                        <td>Initial Deposit Date</td>-->
-<!--                        <td>Interest Rate</td>-->
-<!--                        <td>Monthly Deposit</td>-->
-<!--                        <td>Days Till Account Maturity</td>-->
-<!--                        <td>Interest After <strong>--><?php //echo $date1->diff($date2)->format("%m"); ?><!--</strong> Months</td>-->
-<!--                        <td>Balance After <strong>--><?php //echo $date1->diff($date2)->format("%m"); ?><!--</strong> Months</td>-->
-<!--                    </tr>-->
-<!--                </tread>-->
-<!--                <tbody>-->
-<!--                <tr style="background-color: rgba(94, 101, 255, 0.29); text-align: center;">-->
-<!--                    <td><strong>--><?php //echo $newDate; ?><!--</strong></td>-->
-<!--                    <td><strong>--><?php //echo $row->interest * 100; ?><!--%</strong> Interest</td>-->
-<!--                    <td><strong>£--><?php //echo $row->monthly_deposits; ?><!--</strong> (PCM)</td>-->
-<!--                    <td><strong>--><?php //echo $remain; ?><!--</strong> Days Left</td>-->
-<!--                    <td><strong>£--><?php //echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?><!--</strong> Interest</td>-->
-<!--                    <td><strong>£--><?php //echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?><!--</strong></td>-->
-<!--                </tr>-->
-<!--                </tbody>-->
-<!--            </table>-->
-<!--    </div>-->
+            $join_date = $row->start_date;
+            $date1 = new DateTime('now');
+            $date2 = new DateTime($join_date);
+
+            $p = $row->start_amount;
+            $i = $row->interest;
+            $c = 12; // compound frequency set to monthly
+            $n = ((int)$date1->diff($date2)->format("%m")) / 12;
+            $r = $row->monthly_deposits;
+            $x = $i / $c;
+            $y = pow((1 + $x), ($n * $c));
+            $Total_balance = $p * $y + ($r * (1 + $x) * ($y - 1) / $x);
+
+            $remain = 365 - $date1->diff($date2)->format("%a days");
+
+            $Total_Deposits = ($row->monthly_deposits * (int)$date1->diff($date2)->format("%m")) + $row->start_amount;
+            $Total_Int = $Total_balance - $Total_Deposits;
+
+            $originalDate = $row->start_date;
+            $newDate = date("jS \of F Y", strtotime($originalDate));
+
+            // Add field values to get row total
+            $rowTotal = $Total_balance;
+
+            // Add row total to grand total
+            $grandTotal += $rowTotal;
+            ?>
+            <div id="desktop-content">
+    <h3>Account: <?php echo $row->bank_name; ?></h3>
+            <table border="1">
+                <tread>
+                    <tr style="background: rgba(134, 240, 133, 0.31); text-align: center;">
+                        <td>Initial Deposit Date</td>
+                        <td>Interest Rate</td>
+                        <td>Monthly Deposit</td>
+                        <td>Days Till Account Maturity</td>
+                        <td>Interest After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
+                        <td>Balance After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
+                    </tr>
+                </tread>
+                <tbody>
+                <tr style="background-color: rgba(94, 101, 255, 0.29); text-align: center;">
+                    <td><strong><?php echo $newDate; ?></strong></td>
+                    <td><strong><?php echo $row->interest * 100; ?>%</strong> Interest</td>
+                    <td><strong>£<?php echo $row->monthly_deposits; ?></strong> (PCM)</td>
+                    <td><strong><?php echo $remain; ?></strong> Days Left</td>
+                    <td><strong>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></strong> Interest</td>
+                    <td><strong>£<?php echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?></strong></td>
+                </tr>
+                </tbody>
+            </table>
+    </div>
     <div id="desktop-content">
         <table class="table table-hover">
             <thead>
@@ -66,52 +66,25 @@
                 <td>Interest Rate</td>
                 <td>Monthly Deposit</td>
                 <td>Days Till Account Maturity</td>
-<!--                <td>Interest After <strong>--><?php //echo $date1->diff($date2)->format("%m"); ?><!--</strong> Months</td>-->
-<!--                <td>Balance After <strong>--><?php //echo $date1->diff($date2)->format("%m"); ?><!--</strong> Months</td>-->
+                <td>Interest After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
+                <td>Balance After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
             </tr>
             </thead>
 
             <tbody>
 
-            <?php if (isset($records)) : foreach ($records as $row) :
-                $join_date = $row->start_date;
-                $date1 = new DateTime('now');
-                $date2 = new DateTime($join_date);
-
-                $p = $row->start_amount;
-                $i = $row->interest;
-                $c = 12; // compound frequency set to monthly
-                $n = ((int)$date1->diff($date2)->format("%m")) / 12;
-                $r = $row->monthly_deposits;
-                $x = $i / $c;
-                $y = pow((1 + $x), ($n * $c));
-                $Total_balance = $p * $y + ($r * (1 + $x) * ($y - 1) / $x);
-
-                $remain = 365 - $date1->diff($date2)->format("%a days");
-
-                $Total_Deposits = ($row->monthly_deposits * (int)$date1->diff($date2)->format("%m")) + $row->start_amount;
-                $Total_Int = $Total_balance - $Total_Deposits;
-
-                $originalDate = $row->start_date;
-                $newDate = date("jS \of F Y", strtotime($originalDate));
-
-                // Add field values to get row total
-                $rowTotal = $Total_balance;
-
-                // Add row total to grand total
-                $grandTotal += $rowTotal;
-                ?>
+            <?php if (isset($records)) : foreach ($records as $row) : ?>
                 <tr>
                     <td><?php echo $row->bank_name; ?></td>
-                    <td>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></td>
-                    <td>£<?php echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?></td>
+                    <td><?php echo $row->bank_balance; ?></td>
+                    <td><?php echo $row->bank_start_date; ?></td>
                 </tr>
             <?php endforeach; ?>
             </tbody>
         </table>
     </div>
 
-
+        <?php endforeach; ?>
         <?php endif; ?>
 
 <!--    MOBILE VIEW START-->
@@ -120,12 +93,9 @@
         <table class="table table-hover">
             <thead>
             <tr>
-                <td>Initial Deposit Date</td>
-                <td>Interest Rate</td>
-                <td>Monthly Deposit</td>
-<!--                <td>Days Till Account Maturity</td>-->
-<!--                <td>Interest After <strong>--><?php //echo $date1->diff($date2)->format("%m"); ?><!--</strong> Months</td>-->
-<!--                <td>Balance After <strong>--><?php //echo $date1->diff($date2)->format("%m"); ?><!--</strong> Months</td>-->
+                <th>Account Name</th>
+                <th>Current Interest</th>
+                <th>Current Balance</th>
             </tr>
             </thead>
 
