6c3b159bf92f82c934e14b4f031c6daee965b00b
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 23:53:40 +0100

    testing

--- application/views/test_view.php
+++ application/views/test_view.php
@@ -6,24 +6,13 @@
         <table class="table table-striped">
             <thead>
             <tr>
-                <?php if (isset($records)) :
-                foreach ($records as $row) :
-                $join_date = $row->start_date;
-                $date1 = new DateTime('now');
-                $date2 = new DateTime($join_date);
-
-                    endforeach;
-                endif;
-                ?>
-                ?>
                 <td>Initial Deposit Date</td>
                 <td>Interest Rate</td>
                 <td>Monthly Deposit</td>
                 <td>Days Till Account Maturity</td>
-                <td>Interest After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months
-                </td>
-                <td>Balance After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months
-                </td>
+                <td>Interest</td>
+                <td>Balance</td>
+
             </tr>
             </thead>
             <tbody>
@@ -57,10 +46,12 @@
                 $grandTotal += $rowTotal;
                 ?>
                 <tr>
-                    <td><?php echo $row->bank_name; ?></td>
-                    <td>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></td>
-                    <td>£<?php echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?></td>
-                    <td><?php echo $remain; ?></td>
+                    <td><strong><?php echo $newDate; ?></strong></td>
+                    <td><strong><?php echo $row->interest * 100; ?>%</strong> Interest</td>
+                    <td><strong>£<?php echo $row->monthly_deposits; ?></strong> (PCM)</td>
+                    <td><strong><?php echo $remain; ?></strong> Days Left</td>
+                    <td><strong>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></strong> After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
+                    <td><strong>£<?php echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?></strong>After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months</td>
                 </tr>
             <?php endforeach; ?>
             </tbody>
