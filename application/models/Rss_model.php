b5be2ec6f3042639b1bacb54db60d83d55a64338
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 01:10:24 +0100

    sorting out the queies

--- application/models/Rss_model.php
+++ application/models/Rss_model.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH'))
     exit('No direct script access allowed');
-class feed_model extends Model
+class feed_model extends CI_Model
 {
     // get all
     function get_feeds()
