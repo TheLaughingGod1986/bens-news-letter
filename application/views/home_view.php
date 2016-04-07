bd0d6031a0c791183f037a331a2735f3d3773a16
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 01:37:50 +0100

    sorting out the queies

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -14,10 +14,9 @@
                 if ($this->session->userdata('id')) { ?>
             <strong>Welcome Back, <?php echo $this->session->userdata('username'); ?></strong>
             <?php
-            } else { ?>
-                <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
-
-                <?php
+            } else {
+               echo "<a class='btn btn-lg btn-primary'  echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>";
+                    
             }
             ?>
 
@@ -27,18 +26,18 @@
 <!-- Example row of columns -->
 <div class="row">
     <div class="col-lg-4">
-        <h2>Safari bug warning!</h2>
+        <h2>Customizable Dashboard</h2>
         <p class="text-danger">As of v8.0, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
         <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
     </div>
     <div class="col-lg-4">
-        <h2>Heading</h2>
+        <h2>Accounts Overview</h2>
         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
         <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
     </div>
     <div class="col-lg-4">
-        <h2>Heading</h2>
+        <h2>Add New Accounts</h2>
         <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
         <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
     </div>
