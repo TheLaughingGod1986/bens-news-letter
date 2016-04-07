b5f414a8ec6fc3dee467bdec514c628dc741ab97
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 01:50:03 +0100

    sorting out home page

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,82 +1,88 @@
 <!--<div id="desktop-content">-->
-    <div class="container-fluid">
-        <div class="jumbotron">
-            <h1>Servare</h1>
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h1>Servare</h1>
 
-            <div id="desktop-content">
-                <span>Tool to help you save</span>
-                <p>here i will write about the product. will hide this if already a member and show alternative. this
-                    can only be viewd on desktop</p>
-            </div>
+        <div id="desktop-content">
+            <span>Tool to help you save</span>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
+                condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+                euismod. Donec sed odio dui. </p>
+        </div>
+        <p>
+
+            <?php
+            if ($this->session->userdata('id')) { ?>
+                <strong>Welcome Back, <?php echo $this->session->userdata('username'); ?></strong>
+                <?php
+            } else { ?>
+                <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
+
+                <?php
+            }
+            ?>
+
+        </p>
+    </div>
+
+    <!-- Example row of columns -->
+    <div class="row">
+        <div class="col-lg-4">
+            <h2>Customizable Dashboard</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
+                condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+                euismod. Donec sed odio dui. </p>
             <p>
-
                 <?php
                 if ($this->session->userdata('id')) { ?>
-            <strong>Welcome Back, <?php echo $this->session->userdata('username'); ?></strong>
-            <?php
-            } else { ?>
-                <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
+                    <a class="btn btn-lg btn-primary" <?php echo anchor('members', 'My Dashboard &raquo;'); ?> </a>
+                    <?php
+                } else { ?>
+                    <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
 
-                <?php
-            }
-            ?>
-
+                    <?php
+                }
+                ?>
             </p>
         </div>
+        <div class="col-lg-4">
+            <h2>Accounts Overview</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
+                condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+                euismod. Donec sed odio dui. </p>
+            <p>
+                <?php
+                if ($this->session->userdata('id')) { ?>
+                    <a class="btn btn-lg btn-primary" <?php echo anchor('profile', 'My Account Overview &raquo;'); ?> </a>
+                    <?php
+                } else { ?>
+                    <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
 
-<!-- Example row of columns -->
-<div class="row">
-    <div class="col-lg-4">
-        <h2>Customizable Dashboard</h2>
-        <p class="text-danger">As of v8.0, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p>
-            <?php
-            if ($this->session->userdata('id')) { ?>
-                <a class="btn btn-lg btn-primary" <?php echo anchor('members', 'My Dashboard &raquo;'); ?> </a>
+                    <?php
+                }
+                ?>
+            </p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Add New Accounts</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
+                porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
+                fermentum massa.</p>
+            <p>
                 <?php
-            } else { ?>
-                <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
+                if ($this->session->userdata('id')) { ?>
+                    <a class="btn btn-lg btn-primary" <?php echo anchor('banks', 'Add Regular Savings &raquo;'); ?> </a>
+                    <?php
+                } else { ?>
+                    <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
 
-                <?php
-            }
-            ?>
-        </p>
-    </div>
-    <div class="col-lg-4">
-        <h2>Accounts Overview</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p>
-            <?php
-            if ($this->session->userdata('id')) { ?>
-                <a class="btn btn-lg btn-primary" <?php echo anchor('profile', 'My Account Overview &raquo;'); ?> </a>
-                <?php
-            } else { ?>
-                <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
-
-                <?php
-            }
-            ?>
-        </p>
-    </div>
-    <div class="col-lg-4">
-        <h2>Add New Accounts</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-        <p>
-            <?php
-            if ($this->session->userdata('id')) { ?>
-                <a class="btn btn-lg btn-primary" <?php echo anchor('banks', 'Add Regular Savings &raquo;'); ?> </a>
-                <?php
-            } else { ?>
-                <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
-
-                <?php
-            }
-            ?>
-        </p>
+                    <?php
+                }
+                ?>
+            </p>
+        </div>
     </div>
 </div>
-        </div>
 <!--</div>-->
 
 <!--<div id="mobile-content">-->
