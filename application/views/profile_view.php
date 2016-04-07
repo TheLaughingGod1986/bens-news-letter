a167e2e8e0fa51d6586bc6edce3557d027eab647
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 16:18:31 +0100

    sorting out the queies

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -3,8 +3,8 @@
     <hr>
         <?php $grandTotal = 0; ?>
         <?php
-        if (isset($records)) :
-        foreach ($records as $row) :
+//        if (isset($records)) :
+//        foreach ($records as $row) :
 
             $join_date = $row->start_date;
             $date1 = new DateTime('now');
@@ -91,5 +91,5 @@
             <h4>Why No Add A Account?</h4>
         <?php endif; ?>
 </div>
-<?php endforeach; ?>
-<?php endif; ?>
+<?php //endforeach; ?>
+<?php //endif; ?>
