70ca554f1ad5e9f733dc0b756910d52ee848099e
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 01:25:29 +0100

    sorting out the queies

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -51,8 +51,6 @@
                     } else { ?>
                         <li><?php echo anchor('login', 'Login'); ?></li>
                         <li><?php echo anchor('login/signup', 'Sign up'); ?></li>
-                        <li><?php echo anchor('feed', 'feed'); ?></li>
-
                         <?php
                     }
                     ?>
