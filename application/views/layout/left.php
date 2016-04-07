74bb0d897f1b252714a06ce5948e414b9209c459
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 20:17:48 +0100

    sorting out front page

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -2,9 +2,4 @@
     <li><a <?php echo anchor('members', 'Your Dashboard'); ?></a></li>
     <li><a <?php echo anchor('profile', 'Account Overview'); ?></a></li>
     <li><a <?php echo anchor('banks', 'Add Regular Savings'); ?></a></li>
-
-    <li><p>Coming Soon...</p></li>
-    <li><p>Easy Access Savings</p></li>
-    <li><p>Notification Accounts</p></li>
-    <li><p>Fixed rate Bonds</p></li>
 </ul>
