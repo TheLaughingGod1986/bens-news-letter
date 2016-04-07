5b122c7c3a00d1b96461671a8bf904bf14be475e
TheLaughingGod1986 <benoats@gmail.com>
2016-04-08 00:55:09 +0100

    testing

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,4 +1,5 @@
 <ul class="nav nav-sidebar">
+    <li><a <?php echo anchor('home', 'Home'); ?></a></li>
     <li><a <?php echo anchor('members', 'Your Dashboard'); ?></a></li>
     <li><a <?php echo anchor('profile', 'Account Overview'); ?></a></li>
     <li><a <?php echo anchor('banks', 'Add Regular Savings'); ?></a></li>
