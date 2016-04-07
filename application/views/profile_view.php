2d7f2692031b2be36d7fa2cdeedac53e38654a3a
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 18:00:25 +0100

    sorting out the queies

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -76,7 +76,34 @@
 
             <tbody>
 
-            <?php if (isset($records)) : foreach ($records as $row) : ?>
+            <?php if (isset($records)) : foreach ($records as $row) : 
+                $join_date = $row->start_date;
+                $date1 = new DateTime('now');
+                $date2 = new DateTime($join_date);
+
+                $p = $row->start_amount;
+                $i = $row->interest;
+                $c = 12; // compound frequency set to monthly
+                $n = ((int)$date1->diff($date2)->format("%m")) / 12;
+                $r = $row->monthly_deposits;
+                $x = $i / $c;
+                $y = pow((1 + $x), ($n * $c));
+                $Total_balance = $p * $y + ($r * (1 + $x) * ($y - 1) / $x);
+
+                $remain = 365 - $date1->diff($date2)->format("%a days");
+
+                $Total_Deposits = ($row->monthly_deposits * (int)$date1->diff($date2)->format("%m")) + $row->start_amount;
+                $Total_Int = $Total_balance - $Total_Deposits;
+
+                $originalDate = $row->start_date;
+                $newDate = date("jS \of F Y", strtotime($originalDate));
+
+                // Add field values to get row total
+                $rowTotal = $Total_balance;
+
+                // Add row total to grand total
+                $grandTotal += $rowTotal;
+                ?>
                 <tr>
                     <td><?php echo $row->bank_name; ?></td>
                     <td>l</td>
