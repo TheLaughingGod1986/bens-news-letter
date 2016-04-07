5f361a05fcc2f93ee30e5a5b23e94f3f0de02a5a
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 02:18:35 +0100

    sorting out home page

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -5,26 +5,43 @@
 
         <div id="desktop-content">
             <p>
-                Welcome to <strong>Servare</strong> A useful tool to help you keep track of your savings accounts no matter what bank all under one roof.
+                Welcome to <strong>Servare</strong> A useful tool to help you keep track of your savings accounts no
+                matter what bank all under one roof.
             </p>
 
             <p>We are very secure due to the fact that we do not ask for any of your account numbers or details. <br>
-                 all we need is just how much you deposit each month, how much interest, when what date was your firs deposit and for you to name the account (this could be Big Bobs Savings). Once we have these you can now track the growth of the account, the total interest earn and how long you have left to account maturity.
+                all we need is just how much you deposit each month, how much interest, when what date was your firs
+                deposit and for you to name the account (this could be Big Bobs Savings). Once we have these you can now
+                track the growth of the account, the total interest earn and how long you have left to account maturity.
             </p>
 
             <p>
-                It really is that easy, and the good news is that once you add all your accounts (and given them wacky names) we will give you your total balance of all your accounts !.
-                You can view this total sum of all your savings combined either on your dashboard or go to <?php
+                It really is that easy, and the good news is that once you add all your accounts (and given them wacky
+                names) we will give you your total balance of all your accounts !.
+                You can view this total sum of all your savings combined either on your <?php
                 if ($this->session->userdata('id')) { ?>
                     <?php echo anchor('profile', 'Account Overview'); ?>
                     <?php
                 } else { ?>
-                   <?php echo "<strong>accounts overview</strong>" ?>
-
+                    <?php echo "<strong>accounts overview</strong>" ?>
+                    <?php
+                }
+                ?> or go to
+                <?php
+                if ($this->session->userdata('id')) { ?>
+                    <?php echo anchor('members', 'Dashboard'); ?>
+                    <?php
+                } else { ?>
+                    <?php echo "<strong>dashboard</strong>" ?>
                     <?php
                 }
                 ?>
                 for a more comprehensive breakdown
+            </p>
+            <p>
+                You can use us on any devise either it be your table on your sofa, your smart phone on your coffee brake
+                or on your desktop.
+                we give different information depending on what device your viewing on !
             </p>
         </div>
         <p>
@@ -35,11 +52,9 @@
                 <?php
             } else { ?>
                 <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
-
                 <?php
             }
             ?>
-
         </p>
     </div>
 
@@ -57,7 +72,6 @@
                     <?php
                 } else { ?>
                     <a class="btn btn-lg btn-primary" <?php echo anchor('login/signup', 'Sign up &raquo;'); ?> </a>
-
                     <?php
                 }
                 ?>
@@ -101,8 +115,3 @@
         </div>
     </div>
 </div>
-<!--</div>-->
-
-<!--<div id="mobile-content">-->
-<!--    mobile view text-->
-<!--</div>-->
