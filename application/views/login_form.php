9cea484c3c5744efb0a22b232655d7f90baa8ccb
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 22:53:19 +0100

    sorting out front page

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,8 +1,7 @@
 <div class="conInformationid">
     <h2>Log In Now!</h2>
     <hr>
-    <div id="desktop-content">
-        <div class="col-sm-5">
+    <div id="desktop-content" class="col-sm-5">
             <?php
             $this->load->helper('html');
             $img = base_url('img/cash-flow.jpg');
@@ -13,9 +12,7 @@
                 echo anchor($path, img($img));
                 ?>
             </div>
-        </div>
 
-        <div class="col-sm-4">
             <div id="login_form">
                 <?php
                 echo form_open('login/validate_credentials');
@@ -38,30 +35,5 @@
 
                 <?php echo validation_errors('<p class="error"/>'); ?>
             </div>
-        </div>
-    </div>
-    <div id="mobile-content">
-        <div id="login_form">
-            <?php
-            echo form_open('login/validate_credentials');
-            $email_addresse = array(
-                'name' => 'email_address',
-                'id' => 'email_address',
-                'placeholder' => 'Please Enter Your Email Address',
-            );
-            $password = array(
-                'name' => 'password',
-                'id' => 'password',
-                'placeholder' => 'Please Enter Your Password',
-            );
-            echo "<legend>Login Details</legend>";
-            echo form_input($email_addresse);
-            echo form_password($password);
-            echo form_submit('submit', 'Login');
-            echo anchor('login/signup', 'Create Account');
-            ?>
-
-            <?php echo validation_errors('<p class="error"/>'); ?>
-        </div>
     </div>
 </div>
