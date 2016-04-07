480af1923a066d4a9e464c1f6597199a9ca6ed59
TheLaughingGod1986 <benoats@gmail.com>
2016-04-07 23:45:35 +0100

    testing

--- application/views/test_view.php
+++ application/views/test_view.php
@@ -6,19 +6,8 @@
         <table class="table table-striped">
             <thead>
             <tr>
-                <td>Initial Deposit Date</td>
-                <td>Interest Rate</td>
-                <td>Monthly Deposit</td>
-                <td>Days Till Account Maturity</td>
-<!--                <td>Interest After <strong>--><?php //echo $date1->diff($date2)->format("%m"); ?><!--</strong> Months-->
-<!--                </td>-->
-<!--                <td>Balance After <strong>--><?php //echo $date1->diff($date2)->format("%m"); ?><!--</strong> Months-->
-<!--                </td>-->
-            </tr>
-            </thead>
-            <tbody>
-            <?php if (isset($records)) :
-            foreach ($records as $row) :
+                <?php if (isset($records)) :
+                foreach ($records as $row) :
                 $join_date = $row->start_date;
                 $date1 = new DateTime('now');
                 $date2 = new DateTime($join_date);
@@ -46,6 +35,18 @@
                 // Add row total to grand total
                 $grandTotal += $rowTotal;
                 ?>
+                <td>Initial Deposit Date</td>
+                <td>Interest Rate</td>
+                <td>Monthly Deposit</td>
+                <td>Days Till Account Maturity</td>
+                <td>Interest After <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months
+                </td>
+<!--                <td>Balance After <strong>--><?php //echo $date1->diff($date2)->format("%m"); ?><!--</strong> Months-->
+<!--                </td>-->
+            </tr>
+            </thead>
+            <tbody>
+
                 <tr>
                     <td><?php echo $row->bank_name; ?></td>
                     <td>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></td>
