2dcafa193f9b1d3d538e165ad572b00cd311a9c4
TheLaughingGod1986 <benoats@gmail.com>
2016-04-08 17:46:24 +0100

    refactoring code

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -6,6 +6,7 @@ class Profile extends MY_Controller
     {
         parent::__construct();
         $this->is_logged_in();
+        $this-&gt;load-&gt;model('test');
     }
 
     function index()
@@ -24,6 +25,7 @@ class Profile extends MY_Controller
 
     function test()
     {
+        $this-&gt;load-&gt;view('chart');
         $data = array();
         $this->load->model('user_profile/profiles_model');
         $query = $this->profiles_model->get_bank();
@@ -38,6 +40,39 @@ class Profile extends MY_Controller
         $this->template['middle'] = $this->load->view($this->middle = 'test_view', $data, true);
         $this->layout();
     }
+    public function data()
+    {
+
+        $data = $this-&gt;data-&gt;get_data();
+
+$category = array();
+$category['name'] = 'Category';
+
+$series1 = array();
+$series1['name'] = 'WordPress';
+
+$series2 = array();
+$series2['name'] = 'Code Igniter';
+
+$series3 = array();
+$series3['name'] = 'Highcharts';
+
+foreach ($data as $row)
+{
+    $category['data'][] = $row-&gt;month;
+$series1['data'][] = $row-&gt;wordpress;
+$series2['data'][] = $row-&gt;codeigniter;
+$series3['data'][] = $row-&gt;highcharts;
+}
+
+$result = array();
+array_push($result,$category);
+array_push($result,$series1);
+array_push($result,$series2);
+array_push($result,$series3);
+
+print json_encode($result, JSON_NUMERIC_CHECK);
+}
 
     public function account_data()
     {
