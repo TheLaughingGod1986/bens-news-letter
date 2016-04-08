b1479249c1a79f3c7e844457fbd856505a29a41c
TheLaughingGod1986 <benoats@gmail.com>
2016-04-08 17:50:09 +0100

    refactoring code

--- application/views/test_view.php
+++ application/views/test_view.php
@@ -1,65 +1,88 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<div class="container-fluid">
+    <h2>Account Overview</h2>
+    <hr>
+    <?php $grandTotal = 0; ?>
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
+            <tr style="background: rgba(134, 240, 133, 0.31); text-align: center;">
+                <td><h4>Account Name</h4></td>
+                <td id="desktop-content"><h4>Initial Deposit Date</h4></td>
+                <td id="desktop-content"><h4>Interest Rate</h4></td>
+                <td id="desktop-content"><h4>Monthly Deposit</h4></td>
+                <td id="desktop-content"><h4>Days Till Account Maturity</h4></td>
+                <td id="desktop-content"><h4>Total Interest</h4></td>
+                <td id="mobile-content"><h4>Interest</h4></td>
+                <td id="desktop-content"><h4>Total Balance</h4></td>
+                <td id="mobile-content"><h4>Balance</h4></td>
+            </tr>
+            </thead>
+            <tbody>
+            <?php if (isset($records)) :
+            foreach ($records as $row) :
+                $join_date = $row->start_date;
+                $date1 = new DateTime('now');
+                $date2 = new DateTime($join_date);
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript">// <![CDATA[
-    $(document).ready(function() {
-        var options = {
-            chart: {
-                renderTo: 'container',
-                type: 'line',
-                marginRight: 130,
-                marginBottom: 25
-            },
-            title: {
-                text: 'Project Requests',
-                x: -20 //center
-            },
-            subtitle: {
-                text: '',
-                x: -20
-            },
-            xAxis: {
-                categories: []
-            },
-            yAxis: {
-                title: {
-                    text: 'Requests'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                formatter: function() {
-                    return '<b>'+ this.series.name +'</b>
-                    '+
-                    this.x +': '+ this.y;
-                }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'top',
-                x: -10,
-                y: 100,
-                borderWidth: 0
-            },
+                $p = $row->start_amount;
+                $i = $row->interest;
+                $c = 12; // compound frequency set to monthly
+                $n = ((int)$date1->diff($date2)->format("%m")) / 12;
+                $r = $row->monthly_deposits;
+                $x = $i / $c;
+                $y = pow((1 + $x), ($n * $c));
+                $Total_balance = $p * $y + ($r * (1 + $x) * ($y - 1) / $x);
 
-            series: []
-        }
+                $remain = 365 - $date1->diff($date2)->format("%a days");
 
-        $.getJSON("data", function(json) {
-            options.xAxis.categories = json[0]['data'];
-            options.series[0] = json[1];
-            options.series[1] = json[2];
-            options.series[2] = json[3];
-            chart = new Highcharts.Chart(options);
-        });
-    });
+                $Total_Deposits = ($row->monthly_deposits * (int)$date1->diff($date2)->format("%m")) + $row->start_amount;
+                $Total_Int = $Total_balance - $Total_Deposits;
 
-    // ]]></script>
-<script type="text/javascript" src="http://code.highcharts.com/highcharts.js"></script>
-<script type="text/javascript" src="http://code.highcharts.com/modules/exporting.js"></script>
-<div id="container" style="min-width: 400px; height: 400px; margin: 0 auto;"></div>
+                $originalDate = $row->start_date;
+                $newDate = date("jS \of F Y", strtotime($originalDate));
+
+                // Add field values to get row total
+                $rowTotal = $Total_balance;
+
+                // Add row total to grand total
+                $grandTotal += $rowTotal;
+                ?>
+                <tr style="background-color: rgba(94, 101, 255, 0.29); text-align: center;">
+                    <td><?php echo $row->bank_name; ?></td>
+                    <td id="desktop-content"><strong><?php echo $newDate; ?></strong></td>
+                    <td id="desktop-content"><strong><?php echo $row->interest * 100; ?>%</strong> Interest</td>
+                    <td id="desktop-content"><strong>£<?php echo $row->monthly_deposits; ?></strong> (PCM)</td>
+                    <td id="desktop-content"><strong><?php echo $remain; ?></strong> Days Left</td>
+                    <td id="desktop-content">
+                        <strong>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></strong><br>
+                        <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months Old
+                    </td>
+                    <td id="mobile-content"><strong>£<?php echo round($Total_Int, 2, PHP_ROUND_HALF_UP); ?></strong>
+                    </td>
+                    <td id="desktop-content">
+                        <strong>£<?php echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?></strong><br>
+                        <strong><?php echo $date1->diff($date2)->format("%m"); ?></strong> Months Old
+                    </td>
+                    <td id="mobile-content"><strong>£<?php echo round($Total_balance, 2, PHP_ROUND_HALF_UP); ?></strong>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+            <tfoot>
+            <tr>
+                <td id="desktop-content" colspan="5"><h4>Total Savings To Date</h4></td>
+                <td id="mobile-content" colspan="2"><h4>Total Savings To Date</h4></td>
+                <td id="desktop-content" colspan="2">
+                    <strong>£<?php echo round($grandTotal, 2, PHP_ROUND_HALF_UP); ?></strong></td>
+                <td id="mobile-content" colspan="1">
+                    <strong>£<?php echo round($grandTotal, 2, PHP_ROUND_HALF_UP); ?></strong></td>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
+    <strong id="mobile-content">For full breakdown of your accounts please view on a Tablet or Desktop</strong>
+<?php else : ?>
+    <h3>You Have No Accounts</h3>
+    <h4>Why No Add A Account?</h4>
+<?php endif; ?>
+</div>
