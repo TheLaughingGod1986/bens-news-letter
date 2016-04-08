56717a359c5b62b67cc6bfe00bcdc1abb1573d27
TheLaughingGod1986 <benoats@gmail.com>
2016-04-08 01:03:57 +0100

    testing

--- application/views/add_bank_form.php
+++ application/views/add_bank_form.php
@@ -57,40 +57,4 @@
                 <?php echo validation_errors('<p class="error"/>'); ?>
             </div>
         </div>
-    
-    <div id="mobile-content">
-        <div id="login_form">
-            <legend>Account Details</legend>
-            <?php
-            echo form_open('banks/create_bank');
-            $account_name = array(
-                'name' => 'bank_name',
-                'id' => 'bank_name',
-                'placeholder' => 'Select Account Name',
-            );
-            $account_interest = array(
-                'name' => 'interest',
-                'id' => 'interest',
-                'placeholder' => 'Select Interest Rate (%)',
-            );
-            $account_deposits = array(
-                'name' => 'monthly_deposits',
-                'id' => 'monthly_deposits',
-                'placeholder' => 'Select Monthly Deposits (£)',
-            );
-            $account_date = array(
-                'name' => 'start_date',
-                'id' => 'datepicker',
-                'placeholder' => 'Select Initial Deposit Date',
-            );
-            echo form_input($account_name);
-            echo form_input($account_interest);
-            echo form_input($account_deposits);
-            echo form_input($account_date);
-            echo form_submit('submit', 'Add Bank ')
-            ?>
-            <?php echo validation_errors('<p class="error"/>'); ?>
-        </div>
     </div>
-</div>
-
